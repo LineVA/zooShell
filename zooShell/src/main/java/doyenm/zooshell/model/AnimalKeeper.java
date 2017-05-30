ab5fee6b72716f6b7dcb1b475d3c5dc5a956d25d
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-05-30 21:11:05 +0200

    Update Add @Builder

--- zooShell/src/main/java/doyenm/zooshell/model/AnimalKeeper.java
+++ zooShell/src/main/java/doyenm/zooshell/model/AnimalKeeper.java
@@ -1,9 +1,14 @@
 package doyenm.zooshell.model;
 
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  *
  * @author doyenm
  */
+@Getter
+@Builder
 public class AnimalKeeper {
 
     private String name;
