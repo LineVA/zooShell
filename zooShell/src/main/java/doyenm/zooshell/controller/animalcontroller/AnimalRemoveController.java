6fcbced8a581b420a5b96f30cd269370877162ad
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-03 22:34:23 +0200

    ADD : Controller to remove an animal

---
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/AnimalRemoveController.java
@@ -0,0 +1,19 @@
+package doyenm.zooshell.controller.animalcontroller;
+
+import doyenm.zooshell.context.AnimalContext;
+import java.util.function.Function;
+
+/**
+ *
+ * @author doyenm
+ */
+public class AnimalRemoveController implements Function<AnimalContext, AnimalContext> {
+
+    @Override
+    public AnimalContext apply(AnimalContext t) {
+        AnimalContext context = t;
+        context.getZoo().getAnimals().remove(context.getAnimal());
+        return context;
+    }
+
+}
