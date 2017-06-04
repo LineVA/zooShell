9cac5b8ec8ce0a154d7c01b8c796378575b0ab56
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-04 14:52:23 +0200

    UPDATE : Split ExecuteReproduction into txo classes - one to compute the gestation duration and see if the animal calves - one for the animal to calve

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/reproduction/ExecuteReproductionFunction.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/reproduction/ExecuteReproductionFunction.java
@@ -1,11 +1,6 @@
 package doyenm.zooshell.controller.animalcontroller.evaluation.reproduction;
 
-import doyenm.zooshell.context.AnimalCreationContext;
 import doyenm.zooshell.context.AnimalEvaluationContext;
-import doyenm.zooshell.model.Animal;
-import doyenm.zooshell.model.Sex;
-import doyenm.zooshell.utils.UniformStatistics;
-import doyenm.zooshell.validator.AnimalCreationValidator;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -16,50 +11,29 @@ import java.util.stream.Stream;
 public class ExecuteReproductionFunction
         implements Function<AnimalEvaluationContext, AnimalEvaluationContext> {
 
-    UniformStatistics uniformStatistics = new UniformStatistics();
-    AnimalCreationValidator animalCreationValidator = new AnimalCreationValidator();
+    private CalvingFunction calvingFunction;
+
+    public ExecuteReproductionFunction() {
+        this.calvingFunction = new CalvingFunction();
+    }
+
+    public ExecuteReproductionFunction(CalvingFunction calvingFunction) {
+        this.calvingFunction = calvingFunction;
+    }
 
     @Override
     public AnimalEvaluationContext apply(AnimalEvaluationContext t) {
-        AnimalEvaluationContext context = t;
-        Animal female = context.getAnimal();
-        int litterSize = determineLitterSize(context.getAnimal());
-        for (int i = 0; i < litterSize; i++) {
-            final int j = i + 1;
-            context.getChildren().add(
-                    Stream.of(context)
-                    .map((AnimalEvaluationContext t1) -> new AnimalCreationContext(t1.getZoo(),
-                                    determineName(female, j), female.getSpecie().getNames().getName(),
-                                    determineSex(), female.getPaddock().getName()))
-                    .filter(animalCreationValidator)
-                    .map(new Function<AnimalCreationContext, Animal>() {
-                        @Override
-                        public Animal apply(AnimalCreationContext t) {
-                           return t.createNewborn();
-                        }
-                    })
+        int currentGestationDuration = t.getMonthsPerTurn() + t.getCurrentGestationDuration();
+        if (currentGestationDuration >= t.getGestationDuration()) {
+            t.setCurrentGestationDuration(0);
+            return Stream.of(t)
+                    .map(calvingFunction)
                     .findFirst()
-                    .get()
-            );
+                    .get();
+        } else {
+            t.setCurrentGestationDuration(currentGestationDuration);
+            return t;
         }
-        female.setNumberOfChildren(female.getNumberOfChildren() + litterSize);
-        return context;
-    }
-
-    private int determineLitterSize(Animal female) {
-        return uniformStatistics.getRandomLowerOrEqualsThan(female.getReproductionAttributes().getLitterSize() * 2);
-    }
-
-    private String determineName(Animal female, int j) {
-        int number = female.getNumberOfChildren() + j;
-        return female.getName() + number;
-    }
-
-    private String determineSex() {
-        if (uniformStatistics.uniform() >= 0.5) {
-            return Sex.MALE.toString();
-        }
-        return Sex.FEMALE.toString();
     }
 
 }
