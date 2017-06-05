05f5fba78df6716833271ee6986a2363f59c10a5
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-05 17:52:17 +0200

    UPDATE : Test about the age controller

--- zooShell/src/test/java/doyenm/zooshell/controller/animalcontroller/evaluation/AnimalAgeEvaluationControllerApplyTest.java
+++ zooShell/src/test/java/doyenm/zooshell/controller/animalcontroller/evaluation/AnimalAgeEvaluationControllerApplyTest.java
@@ -3,6 +3,8 @@ package doyenm.zooshell.controller.animalcontroller.evaluation;
 import doyenm.zooshell.controller.animalcontroller.evaluation.AnimalAgeEvaluationController;
 import doyenm.zooshell.context.AnimalEvaluationContext;
 import doyenm.zooshell.model.Animal;
+import doyenm.zooshell.model.ReproductionAttributes;
+import doyenm.zooshell.model.Sex;
 import doyenm.zooshell.model.Zoo;
 import doyenm.zooshell.testUtils.TestUtils;
 import org.assertj.core.api.Assertions;
@@ -21,14 +23,31 @@ public class AnimalAgeEvaluationControllerApplyTest {
         Mockito.doCallRealMethod().when(animal).setAge(Mockito.any(Integer.class));
         return animal;
     }
-    
-    private Zoo givenZooWithSpeed(int speed){
+
+    private Animal givenAnimalWithAgeMaturityAndNursing(int age, int maturity, boolean nursing) {
+        Animal animal = Mockito.mock(Animal.class);
+        Mockito.when(animal.getAge()).thenReturn(age).thenCallRealMethod();
+        Mockito.doCallRealMethod().when(animal).setAge(Mockito.any(Integer.class));
+        ReproductionAttributes attributes = Mockito.mock(ReproductionAttributes.class);
+        Mockito.when(attributes.getWeaningAge()).thenReturn(maturity);
+        Mockito.when(animal.getReproductionAttributes()).thenReturn(attributes);
+        Mockito.when(animal.isNeedWeaningByHumans()).thenReturn(nursing);
+        return animal;
+    }
+
+    private Zoo givenZooWithSpeed(int speed) {
         Zoo zoo = Mockito.mock(Zoo.class);
         Mockito.when(zoo.getMonthsPerEvaluation()).thenReturn(speed);
         return zoo;
     }
-    
-    private AnimalEvaluationContext givenContextWithAnimalAndZoo(Animal animal, Zoo zoo){
+
+    private Zoo givenZoo() {
+        Zoo zoo = Mockito.mock(Zoo.class);
+        Mockito.when(zoo.getMonthsPerEvaluation()).thenReturn(TestUtils.generateInteger());
+        return zoo;
+    }
+
+    private AnimalEvaluationContext givenContextWithAnimalAndZoo(Animal animal, Zoo zoo) {
         AnimalEvaluationContext context = Mockito.mock(AnimalEvaluationContext.class);
         Mockito.when(context.getZoo()).thenReturn(zoo);
         Mockito.when(context.getAnimal()).thenReturn(animal);
@@ -39,7 +58,7 @@ public class AnimalAgeEvaluationControllerApplyTest {
     public void shouldAddTheNumberOfMonthsPerTurnToTheAgeOfTheAnimal() {
         // Given
         int age = TestUtils.generateInteger();
-        Animal animal = givenAnimalWithAge(age);
+        Animal animal = givenAnimalWithAgeMaturityAndNursing(age, age - 1, false);
         int speed = TestUtils.generateInteger();
         Zoo zoo = givenZooWithSpeed(speed);
         AnimalEvaluationContext context = givenContextWithAnimalAndZoo(animal, zoo);
@@ -49,16 +68,76 @@ public class AnimalAgeEvaluationControllerApplyTest {
         // Then
         Assertions.assertThat(actualContext.getAnimal().getAge()).isEqualTo(age + speed);
     }
-    
+
     /**
      * The animal did not need to be nursing by humans during the current turn
+     * even if it is not yet weaned
      */
     @Test
-    public void shouldLetTheNecessityOfNursingByHumansToFalseWhenTheAnimalDidNotNeedIt(){
+    public void shouldLetTheNecessityOfNursingByHumansToFalseWhenTheAnimalDidNotNeedItAndIsNotYetWeaned() {
         // Given
-        
+        int age = TestUtils.generateInteger();
+        Animal animal = givenAnimalWithAgeMaturityAndNursing(age, age - 1, false);
+        Zoo zoo = givenZoo();
+        AnimalEvaluationContext context = givenContextWithAnimalAndZoo(animal, zoo);
+        AnimalAgeEvaluationController controller = new AnimalAgeEvaluationController();
         // When
+        AnimalEvaluationContext actualContext = controller.apply(context);
         // Then
-        
+        Assertions.assertThat(actualContext.getAnimal().isNeedWeaningByHumans()).isFalse();
+    }
+    
+       /**
+     * The animal need to be nursing by humans during the current turn
+     * beacuse it is weaned, even if it needed it the previous turn
+     */
+    @Test
+    public void shouldSetTheNecessityOfNursingByHumansToTrueWhenTheAnimalIsWeaned() {
+        // Given
+        int age = TestUtils.generateInteger();
+        Animal animal = givenAnimalWithAgeMaturityAndNursing(age, age - 1, true);
+        Zoo zoo = givenZoo();
+        AnimalEvaluationContext context = givenContextWithAnimalAndZoo(animal, zoo);
+        AnimalAgeEvaluationController controller = new AnimalAgeEvaluationController();
+        // When
+        AnimalEvaluationContext actualContext = controller.apply(context);
+        // Then
+        Assertions.assertThat(actualContext.getAnimal().isNeedWeaningByHumans()).isTrue();
+    }
+    
+       /**
+     * The animal did not need to be nursing by humans during the current turn
+     * beacuse it is weaned
+     */
+    @Test
+    public void shouldLetTheNecessityOfNursingByHumansToFalseWhenTheAnimalIsWeaned_2() {
+        // Given
+        int age = TestUtils.generateInteger();
+        Animal animal = givenAnimalWithAgeMaturityAndNursing(age, age + 1, false);
+        Zoo zoo = givenZoo();
+        AnimalEvaluationContext context = givenContextWithAnimalAndZoo(animal, zoo);
+        AnimalAgeEvaluationController controller = new AnimalAgeEvaluationController();
+        // When
+        AnimalEvaluationContext actualContext = controller.apply(context);
+        // Then
+        Assertions.assertThat(actualContext.getAnimal().isNeedWeaningByHumans()).isFalse();
+    }
+    
+       /**
+     * The animal did not need to be nursing by humans during the current turn
+     * beacuse it is weaned, even if it needed it the previous turn
+     */
+    @Test
+    public void shouldSetTheNecessityOfNursingByHumansToTrueWhenTheAnimalIsWeaned_2() {
+        // Given
+        int age = TestUtils.generateInteger();
+        Animal animal = givenAnimalWithAgeMaturityAndNursing(age, age + 1, true);
+        Zoo zoo = givenZoo();
+        AnimalEvaluationContext context = givenContextWithAnimalAndZoo(animal, zoo);
+        AnimalAgeEvaluationController controller = new AnimalAgeEvaluationController();
+        // When
+        AnimalEvaluationContext actualContext = controller.apply(context);
+        // Then
+        Assertions.assertThat(actualContext.getAnimal().isNeedWeaningByHumans()).isTrue();
     }
 }
