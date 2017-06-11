be6ab0529f8181f2c05c7b31214b384a0bb76045
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-11 16:22:11 +0200

    ADD Method to count the animal of the same specie in the paddock

--- zooShell/src/main/java/doyenm/zooshell/context/AnimalEvaluationContext.java
+++ zooShell/src/main/java/doyenm/zooshell/context/AnimalEvaluationContext.java
@@ -8,6 +8,7 @@ import doyenm.zooshell.model.Zoo;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,53 +19,65 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AnimalEvaluationContext {
-    
+
     private final double base = 5.0;
     private final double zero = 0.0;
 
     private final Zoo zoo;
     private Animal animal;
-    private boolean dead = false; 
+    private boolean dead = false;
     private List<Animal> children = new ArrayList<>();
-    
+
     private double biomeWellBeing = 0.0;
-    private double dietsWellBeing = 0.0; 
+    private double dietsWellBeing = 0.0;
     private double foodQuantityWellBeing = 0.0;
+    private double fastDaysWellBeing = 0.0;
+    private double territorySizeWellBeing = 0.0;
+    private double groupSizeWellBeing = 0.0;
 
     public AnimalEvaluationContext(Zoo zoo, Animal animal) {
         this.zoo = zoo;
         this.animal = animal;
     }
-    
-    public Collection<AnimalKeeper> getKeepers(){
-        return getZoo().getKeepers().values();
-    }
-    
-    public int getGestationDuration(){
-        return getAnimal().getReproductionAttributes().getGestationDuration();
-    }
-    
-    public int getMonthsPerTurn(){
-        return getZoo().getMonthsPerEvaluation();
-    }
-    
-    public int getCurrentGestationDuration(){
-        return getAnimal().getMonthsOfGestation();
-    }
-    
-    public void setCurrentGestationDuration(int i){
-         getAnimal().setMonthsOfGestation(i);
+
+    public int getNumberOfAnimalsOfTheSameSpecieAndInTheSamePaddock() {
+        return getAnimalsOfTheZoo()
+                .stream()
+                .filter((Animal a) -> a.getSpecie().equals(getAnimal().getSpecie()))
+                .filter((Animal a) -> a.getPaddock().equals(getAnimal().getPaddock()))
+                .collect(Collectors.toList())
+                .size();
     }
 
-    public List<Animal> getAnimalsOfTheZoo(){
+    public Collection<AnimalKeeper> getKeepers() {
+        return getZoo().getKeepers().values();
+    }
+
+    public int getGestationDuration() {
+        return getAnimal().getReproductionAttributes().getGestationDuration();
+    }
+
+    public int getMonthsPerTurn() {
+        return getZoo().getMonthsPerEvaluation();
+    }
+
+    public int getCurrentGestationDuration() {
+        return getAnimal().getMonthsOfGestation();
+    }
+
+    public void setCurrentGestationDuration(int i) {
+        getAnimal().setMonthsOfGestation(i);
+    }
+
+    public List<Animal> getAnimalsOfTheZoo() {
         return new ArrayList(getZoo().getAnimals().values());
     }
-    
-    public Specie getSpecie(){
+
+    public Specie getSpecie() {
         return getAnimal().getSpecie();
     }
-    
-    public Paddock getPaddock(){
+
+    public Paddock getPaddock() {
         return getAnimal().getPaddock();
     }
 
