7c916f4ea7aa6c675d5f7cf8d26f0eaaad0543d0
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-11 16:01:44 +0200

    Add New field authorizedDeviation

--- zooShell/src/main/java/doyenm/zooshell/model/Uicn.java
+++ zooShell/src/main/java/doyenm/zooshell/model/Uicn.java
@@ -9,20 +9,22 @@ import lombok.Getter;
 @Getter
 public enum Uicn {
 
-    UNKNOWN(0, 1.0),
-    LEAST_CONCERNED(1, 1.1),
-    NEAR_THREATENED(2, 1.2),
-    VULNERABLE(3, 1.3),
-    ENDANGERED(4, 1.4),
-    CRITICALLY_ENDANGERED(5, 1.5),
-    EXTINCT_IN_WILD(6, 1.6),
-    EXTINCT(7, 1.7);
+    UNKNOWN(0, 1.0, 0.5),
+    LEAST_CONCERNED(1, 1.1, 0.2),
+    NEAR_THREATENED(2, 1.2, 0.15),
+    VULNERABLE(3, 1.3, 0.12),
+    ENDANGERED(4, 1.4, 0.10),
+    CRITICALLY_ENDANGERED(5, 1.5, 0.8),
+    EXTINCT_IN_WILD(6, 1.6, 0.5),
+    EXTINCT(7, 1.7, 0.0);
 
     int id;
     double coefficient;
+    double standardDeviation;
     
-    Uicn(int id, double coefficient){
+    Uicn(int id, double coefficient, double deviation){
         this.id = id;
         this.coefficient = coefficient;
+        this.standardDeviation = deviation;
     }
 }
