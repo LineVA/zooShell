646688bdb697f095eb5ddf385ef2c41b9e609da8
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-06-11 16:29:57 +0200

    UPDATE : Compute the global well-being

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/AnimalWellBeingController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/AnimalWellBeingController.java
@@ -2,6 +2,11 @@ package doyenm.zooshell.controller.animalcontroller.evaluation;
 
 import doyenm.zooshell.context.AnimalEvaluationContext;
 import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalBiomeEvaluationController;
+import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalDietsEvaluationController;
+import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalFastDaysEvaluationController;
+import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalFoodQuantityEvaluationController;
+import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalGroupSizeEvaluationController;
+import doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing.AnimalTerritorySizeEvaluationController;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -12,14 +17,39 @@ import java.util.stream.Stream;
 public class AnimalWellBeingController
         implements Function<AnimalEvaluationContext, AnimalEvaluationContext> {
 
-    AnimalBiomeEvaluationController animalBiomeWBEvaluationController
+    AnimalBiomeEvaluationController animalBiomeEvaluationController
             = new AnimalBiomeEvaluationController();
+    AnimalDietsEvaluationController animalDietsEvaluationController
+            = new AnimalDietsEvaluationController();
+    AnimalFoodQuantityEvaluationController animalFoodQuantityEvaluationController
+            = new AnimalFoodQuantityEvaluationController();
+    AnimalFastDaysEvaluationController animalFastDaysEvaluationController
+            = new AnimalFastDaysEvaluationController();
+    AnimalTerritorySizeEvaluationController animalTerritorySizeEvaluationController
+            = new AnimalTerritorySizeEvaluationController();
+    AnimalGroupSizeEvaluationController animalGroupSizeEvaluationController
+            = new AnimalGroupSizeEvaluationController();
 
     @Override
     public AnimalEvaluationContext apply(AnimalEvaluationContext t) {
         AnimalEvaluationContext context = t;
         return Stream.of(context)
-                .map(animalBiomeWBEvaluationController)
+                .map(animalBiomeEvaluationController)
+                .map(animalDietsEvaluationController)
+                .map(animalFastDaysEvaluationController)
+                .map(animalFoodQuantityEvaluationController)
+                .map(animalGroupSizeEvaluationController)
+                .map(animalTerritorySizeEvaluationController)
+                .map((AnimalEvaluationContext t1) -> {
+                    t1.setWellBeing((t1.getBiomeWellBeing()
+                            + t1.getDietsWellBeing()
+                            + t1.getFastDaysWellBeing()
+                            + t1.getFoodQuantityWellBeing()
+                            + t1.getGroupSizeWellBeing()
+                            + t1.getTerritorySizeWellBeing())
+                            / 6.0);
+                    return t1;
+                })
                 .findFirst()
                 .get();
     }
