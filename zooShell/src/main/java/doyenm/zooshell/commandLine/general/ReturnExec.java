60e6a83e62e1bdbe0c0049555b39cd819c213cd8
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-11 20:12:57 +0200

    REFACTO

--- zooShell/src/main/java/doyenm/zooshell/commandLine/general/ReturnExec.java
+++ zooShell/src/main/java/doyenm/zooshell/commandLine/general/ReturnExec.java
@@ -1,25 +1,19 @@
 package doyenm.zooshell.commandLine.general;
 
 import doyenm.zooshell.model.Zoo;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 /**
  *
  * @author doyenm
- */
+ */   
+@Getter
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class ReturnExec {
-    @Getter
     private final String message;
-    @Getter
     private final TypeReturn typeReturn;
-    @Getter
     private Zoo zoo;
-
-    public ReturnExec(String message, TypeReturn typeReturn, Zoo zoo) {
-        this.message = message;
-        this.typeReturn = typeReturn;
-        this.zoo = zoo;
-    }
 }
