a6c515776c94434c9f60714e539b21adf6831b0e
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-11 19:09:28 +0200

    DD Take care of keepers when evaluating

--- zooShell/src/main/java/doyenm/zooshell/controller/zoocontroller/EvaluationController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/zoocontroller/EvaluationController.java
@@ -2,17 +2,21 @@ package doyenm.zooshell.controller.zoocontroller;
 
 import doyenm.zooshell.context.EvaluationContext;
 import doyenm.zooshell.controller.animalcontroller.AnimalEvaluationController;
+import doyenm.zooshell.controller.keepercontroller.KeeperEvaluationController;
 import java.util.function.Function;
 import java.util.stream.Stream;
+import lombok.RequiredArgsConstructor;
 
 /**
  *
  * @author doyenm
  */
+@RequiredArgsConstructor
 public class EvaluationController implements Function<EvaluationContext, EvaluationContext> {
 
-    AnimalEvaluationController animalEvaluationController = new AnimalEvaluationController();
-    ZooEvaluationController zooEvaluationController = new ZooEvaluationController();
+   private final AnimalEvaluationController animalEvaluationController;
+   private final KeeperEvaluationController keeperEvaluationController;
+   private final ZooEvaluationController zooEvaluationController;
 
     @Override
     public EvaluationContext apply(EvaluationContext t) {
@@ -21,6 +25,7 @@ public class EvaluationController implements Function<EvaluationContext, Evaluat
                 .map(zooEvaluationController)
                 //                .map(paddocksEvaluationController)
                 .map(animalEvaluationController)
+                .map(keeperEvaluationController)
                 .map((EvaluationContext t1) -> {
                     t1.setTotalEvaluation(t1.getZooEvaluation() + t1.getPaddocksEvaluation() + t1.getAnimalsEvaluation());
                     return t1;
