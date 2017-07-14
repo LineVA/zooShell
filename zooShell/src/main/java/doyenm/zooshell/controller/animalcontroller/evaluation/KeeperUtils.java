5cda0b04b018cc69217a77ecaafd0fa04146821c
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-14 11:13:45 +0200

    ADD utils method on keepers

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/KeeperUtils.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/KeeperUtils.java
@@ -4,7 +4,9 @@ import doyenm.zooshell.model.AnimalKeeper;
 import doyenm.zooshell.model.Paddock;
 import doyenm.zooshell.model.TaskType;
 import doyenm.zooshell.model.TimedOccupation;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -22,11 +24,22 @@ public class KeeperUtils {
                 .collect(Collectors.toList()).isEmpty();
     }
 
-      public List<TimedOccupation> listOfTimedOccupationInGivenPaddock(AnimalKeeper keeper, Paddock paddock){
-       return keeper.getOccupations()
+    public List<TimedOccupation> listOfTimedOccupationInGivenPaddock(AnimalKeeper keeper, Paddock paddock) {
+        return keeper.getOccupations()
                 .stream()
                 .filter((TimedOccupation t) -> paddock == t.getPaddock())
                 .filter((TimedOccupation t) -> 0.0 != t.getTime())
                 .collect(Collectors.toList());
+    }
+
+    public double timeSpentDoingTheTaskInThePaddock(AnimalKeeper keeper,
+            TaskType task, Paddock paddock) {
+        return keeper.getOccupations()
+                .stream()
+                .filter((TimedOccupation occupation) -> task == occupation.getTaskType())
+                .filter((TimedOccupation occupation) -> paddock == occupation.getPaddock())
+                .map(TimedOccupation::getTime)
+                .findFirst()
+                .get();
     }
 }
