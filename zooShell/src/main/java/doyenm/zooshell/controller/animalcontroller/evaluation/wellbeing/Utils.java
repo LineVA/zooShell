846223e34f4d208afabd07e10588430a773d7a65
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-16 18:46:24 +0200

    ADD Tests aout the utils methods to compute the well being

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/wellbeing/Utils.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/wellbeing/Utils.java
@@ -7,14 +7,17 @@ package doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing;
 public class Utils {
 
     public double computeDeviationBetweenCurrentAndOptimal(double current, double optimal) {
+        if (optimal == 0.0) {
+            return 0.0;
+        }
         if (current >= optimal) {
             return (current - optimal) / optimal;
         }
         return (optimal - current) / optimal;
     }
-    
-    public boolean isBetweenAuthorizedValues(double currentDeviation, double maximalDeviation){
+
+    public boolean isBetweenAuthorizedValues(double currentDeviation, double maximalDeviation) {
         return currentDeviation <= maximalDeviation;
     }
-    
+
 }
