429b3cbc0f2d0a031a82a0484cfa95e94d9c3392
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-24 22:54:33 +0200

    ADD a condition to add a keeper related to the number of paddocks + tests + REFACTO

--- zooShell/src/main/java/doyenm/zooshell/validator/KeeperCreationValidator.java
+++ zooShell/src/main/java/doyenm/zooshell/validator/KeeperCreationValidator.java
@@ -1,23 +1,30 @@
 package doyenm.zooshell.validator;
 
 import doyenm.zooshell.context.KeeperCreationContext;
+import doyenm.zooshell.validator.predicates.KeepersNumberPredicate;
 import doyenm.zooshell.validator.predicates.StringLengthPredicates;
 import doyenm.zooshell.validator.predicates.UniquenessNamesBiPredicates;
 import java.util.function.Predicate;
+import lombok.RequiredArgsConstructor;
 
 /**
  *
  * @author doyenm
  */
+@RequiredArgsConstructor
 public class KeeperCreationValidator implements Predicate<KeeperCreationContext> {
 
-    StringLengthPredicates stringLengthPredicates = new StringLengthPredicates();
-    UniquenessNamesBiPredicates uniquenessNamesBiPredicates = new UniquenessNamesBiPredicates();
-
+    private final StringLengthPredicates stringLengthPredicates;
+    private final UniquenessNamesBiPredicates uniquenessNamesBiPredicates;
+    private final KeepersNumberPredicate keepersNumberPredicate;
+    
     @Override
     public boolean test(KeeperCreationContext t) {
         KeeperCreationContext context = t;
-        return this.stringLengthPredicates.mustBeLowerOrEqualsThan(context.getKeeper(), 50)
+        
+        return 
+                this.keepersNumberPredicate.test(context)
+                & this.stringLengthPredicates.mustBeLowerOrEqualsThan(context.getKeeper(), 50)
                 & this.uniquenessNamesBiPredicates.test(context.getKeeper().toUpperCase(), context.getKeepers().keySet());
     }
 
