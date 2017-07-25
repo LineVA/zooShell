6a03c731b203c287f841f97d7211b8fa4b8a3ccb
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-25 22:46:46 +0200

    ADD Initialisation of the age when creating the paddock
    + tests

--- zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockCreationController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockCreationController.java
@@ -1,6 +1,12 @@
 package doyenm.zooshell.controller.paddockcontroller;
 
 import doyenm.zooshell.context.PaddockCreationContext;
+import doyenm.zooshell.model.Biome;
+import doyenm.zooshell.model.Coordinates;
+import doyenm.zooshell.model.Paddock;
+import doyenm.zooshell.model.PaddockType;
+import doyenm.zooshell.model.Position;
+import java.util.ArrayList;
 import java.util.function.Function;
 
 /**
@@ -12,8 +18,25 @@ public class PaddockCreationController
 
     @Override
     public PaddockCreationContext apply(PaddockCreationContext t) {
-        t.build();
-        return t;
+        PaddockCreationContext context = t;
+        Position position = Position.builder()
+                .x(context.getConvertedX())
+                .y(context.getConvertedY())
+                .build();
+        Coordinates coordinates = Coordinates.builder()
+                .height(context.getConvertedHeight())
+                .width(context.getConvertedWidth())
+                .position(position)
+                .build();
+        Paddock paddock = Paddock.builder()
+                .name(context.getName())
+                .coordinates(coordinates)
+                .extensions(new ArrayList<Coordinates>())
+                .biome(Biome.NONE)
+                .type(PaddockType.UNKNOWN)
+                .build();
+        context.getZoo().getPaddocks().put(context.getName().toUpperCase(), paddock);
+        return context;
     }
 
 }
--- zooShell/src/test/java/doyenm/zooshell/controller/paddockcontroller/PaddockCreationControllerApplyTest.java
+++ zooShell/src/test/java/doyenm/zooshell/controller/paddockcontroller/PaddockCreationControllerApplyTest.java
@@ -1,9 +1,14 @@
 package doyenm.zooshell.controller.paddockcontroller;
 
 import doyenm.zooshell.context.PaddockCreationContext;
+import doyenm.zooshell.model.Biome;
+import doyenm.zooshell.model.Paddock;
+import doyenm.zooshell.model.PaddockType;
 import doyenm.zooshell.model.Zoo;
+import java.util.HashMap;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.lang.math.RandomUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -20,23 +25,46 @@ public class PaddockCreationControllerApplyTest {
         Mockito.when(context.getConvertedWidth()).thenReturn(width);
         Mockito.when(context.getConvertedHeight()).thenReturn(height);
         Mockito.when(context.getName()).thenReturn(name);
-        Mockito.when(context.getZoo()).thenCallRealMethod();
+        Mockito.when(context.getZoo()).thenReturn(zoo);
 //        Mockito.doCallRealMethod().when(context).setZoo(Mockito.any(Zoo.class));
 //        context.(zoo);
         return context;
     }
 
-//    @Test
-//    public void shouldAddToTheZooAPaddcokWithTheExpectedValues() {
-//        // Given
-//        String name = RandomStringUtils.random(10);
-//        int x = RandomUtils.nextInt();
-//        int y = RandomUtils.nextInt();
-//        int width = RandomUtils.nextInt();
-//        int height = RandomUtils.nextInt();
-//        PaddockCreationContext context = givenContext(name, x, y, width, height);
-//        PaddockCreationController controller = new PaddockCreationController();
-//        // When
-//        // Then
-//    }
+    private Zoo givenZoo() {
+        Zoo zoo = Mockito.mock(Zoo.class);
+        Mockito.when(zoo.getPaddocks()).thenCallRealMethod();
+        Mockito.doCallRealMethod().when(zoo).setPaddocks(Mockito.anyMap());
+        zoo.setPaddocks(new HashMap<>());
+        return zoo;
+    }
+
+    @Test
+    public void shouldAddToTheZooAPaddockWithTheExpectedValues() {
+        // Given
+        String name = RandomStringUtils.random(10);
+        int x = RandomUtils.nextInt();
+        int y = RandomUtils.nextInt();
+        int width = RandomUtils.nextInt();
+        int height = RandomUtils.nextInt();
+        Zoo zoo = givenZoo();
+        PaddockCreationContext context = givenContext(zoo, name, x, y, width, height);
+        PaddockCreationController controller = new PaddockCreationController();
+        // When
+        PaddockCreationContext actual = controller.apply(context);
+        // Then
+        Assertions.assertThat(actual.getZoo()).isNotNull();
+        Assertions.assertThat(actual.getZoo().getPaddocks()).isNotNull();
+        Assertions.assertThat(actual.getZoo().getPaddocks().size()).isEqualTo(1);
+        Assertions.assertThat(actual.getZoo().getPaddocks().containsKey(name)).isTrue();
+        Paddock pad = actual.getZoo().getPaddocks().get(name);
+        Assertions.assertThat(pad.getAge()).isEqualTo(0);
+        Assertions.assertThat(pad.getX()).isEqualTo(x);
+        Assertions.assertThat(pad.getY()).isEqualTo(y);
+        Assertions.assertThat(pad.getWidth()).isEqualTo(width);
+        Assertions.assertThat(pad.getHeight()).isEqualTo(height);
+        Assertions.assertThat(pad.getName()).isEqualTo(name);
+        Assertions.assertThat(pad.getBiome()).isEqualTo(Biome.NONE);
+        Assertions.assertThat(pad.getType()).isEqualTo(PaddockType.UNKNOWN);
+    }
 }
