b75b994520111eed7e564f23f28e0ad312b79d3a
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-07-30 21:41:24 +0200

    ADD Annotation to save the empty list of occupations

--- zooShell/src/main/java/doyenm/zooshell/model/AnimalKeeper.java
+++ zooShell/src/main/java/doyenm/zooshell/model/AnimalKeeper.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,6 +27,7 @@ public class AnimalKeeper {
 
     private String name;
     private int age;
+    @XmlElementWrapper
     private List<TimedOccupation> occupations;
     private Map<TaskType, Double> taskEvaluations;
     private Map<Family, Double> familyEvaluations;
