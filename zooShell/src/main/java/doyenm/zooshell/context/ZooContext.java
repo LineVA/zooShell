72de6a319c3e7e2bf1a3a3ebc03807a0783784ef
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-19 22:19:40 +0200

    ADD Method for couples with double

--- zooShell/src/main/java/doyenm/zooshell/context/ZooContext.java
+++ zooShell/src/main/java/doyenm/zooshell/context/ZooContext.java
@@ -18,27 +18,31 @@ public class ZooContext {
     private Zoo zoo;
     private List<Couple> couples = new ArrayList<>();
     private String saveName;
-    
-    public ZooContext(Zoo zoo, String saveName){
+
+    public ZooContext(Zoo zoo, String saveName) {
         this.zoo = zoo;
         this.saveName = saveName;
     }
-    
-    public ZooContext(String saveName){
+
+    public ZooContext(String saveName) {
         this.zoo = null;
         this.saveName = saveName;
     }
-    
-    public ZooContext(Zoo zoo){
+
+    public ZooContext(Zoo zoo) {
         this.zoo = zoo;
         this.saveName = "";
     }
-    
-    public void addCouple(String key, String value){
+
+    public void addCouple(String key, String value) {
         couples.add(new Couple(key, value));
     }
-    
-     public void addCouple(String key, int value){
+
+    public void addCouple(String key, int value) {
+        couples.add(new Couple(key, String.valueOf(value)));
+    }
+
+    public void addCouple(String key, double value) {
         couples.add(new Couple(key, String.valueOf(value)));
     }
 }
