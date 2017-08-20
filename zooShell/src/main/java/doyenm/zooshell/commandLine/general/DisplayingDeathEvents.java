fe578da45842e75656226d65922ca41279c07e43
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 15:10:14 +0200

    ADD Interface to handle events message

---
+++ zooShell/src/main/java/doyenm/zooshell/commandLine/general/DisplayingDeathEvents.java
@@ -0,0 +1,22 @@
+package doyenm.zooshell.commandLine.general;
+
+import doyenm.zooshell.controller.eventhandling.Event;
+import doyenm.zooshell.controller.eventhandling.EventCategory;
+import java.text.MessageFormat;
+
+/**
+ *
+ * @author doyenm
+ */
+public class DisplayingDeathEvents implements DisplayingEvents{
+
+    @Override
+    public boolean canFormat(Event event) {
+        return EventCategory.DEATH == event.getEventType().getCategory();
+    }
+
+    @Override
+    public String format(Event event) {
+        return MessageFormat.format(event.getEventType().getMessage(), event.getSubject().getName());
+    }
+}
