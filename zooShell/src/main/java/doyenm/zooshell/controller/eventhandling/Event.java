ca5a38a26982c4bc30c37042e76e13765c555368
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 14:55:16 +0200

    ADD The constructors dependof the category of the eventType

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/Event.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/Event.java
@@ -1,19 +1,42 @@
 package doyenm.zooshell.controller.eventhandling;
 
 import doyenm.zooshell.model.Animal;
-import lombok.AllArgsConstructor;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 /**
  *
  * @author doyenm
  */
-@RequiredArgsConstructor
-@AllArgsConstructor
 @Getter
 public class Event {
+
     private final EventType eventType;
     private final Animal subject;
     private Animal mother;
+
+    private final List<EventCategory> justSubject = Arrays.asList(EventCategory.DEATH);
+    private final List<EventCategory> subjectAndMother = Arrays.asList(EventCategory.BIRTH);
+
+    public Event(EventType eventType, Animal subject) {
+        if (justSubject.contains(eventType.getCategory())) {
+            this.eventType = eventType;
+            this.subject = subject;
+        } else {
+            this.eventType = null;
+            this.subject = null;
+        }
+    }
+    
+      public Event(EventType eventType, Animal subject, Animal mother) {
+        if (subjectAndMother.contains(eventType.getCategory())) {
+            this.eventType = eventType;
+            this.subject = subject;
+        } else {
+            this.eventType = null;
+            this.subject = null;
+        }
+    }
+
 }
