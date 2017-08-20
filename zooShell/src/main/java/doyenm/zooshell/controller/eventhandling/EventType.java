3d8fe44cadb7fc0c06ab45adf4b0d3da66c5c423
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 14:55:01 +0200

    ADD Add Category to EventType

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
@@ -8,17 +8,21 @@ import lombok.Getter;
  */
 public enum EventType {
 
-    DEATH_OF_AGE("The animal {0} is dead of old age"),
-    DEATH_OF_DROWN("The animal {0} is dead of drown"),
-    DEATH_OF_HUNGER("The animal {0} is dead of starvation"),
-    DIYING_OF_UNGER("The animal {0} is dying of starvation"),
-    DIYING_OF_DROWN("The animal {0} is diying of drowning"),
-    BIRTH("The animal {0} is born ; its mother is {1}");
+    DEATH_OF_AGE(EventCategory.DEATH, "The animal {0} is dead of old age"),
+    DEATH_OF_DROWN(EventCategory.DEATH, "The animal {0} is dead of drown"),
+    DEATH_OF_HUNGER(EventCategory.DEATH, "The animal {0} is dead of starvation"),
+    DIYING_OF_UNGER(EventCategory.DEATH, "The animal {0} is dying of starvation"),
+    DIYING_OF_DROWN(EventCategory.DEATH, "The animal {0} is diying of drowning"),
+    BIRTH(EventCategory.BIRTH, "The animal {0} is born ; its mother is {1}");
+    
+    @Getter
+    private EventCategory category;
     
     @Getter
     private final String message;
 
-    private EventType(String message) {
+    private EventType(EventCategory category, String message) {
+        this.category = category;
         this.message = message;
     }
 }
