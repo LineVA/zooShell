f54591e02cf9531e038d99bb1250edda3ccfe31d
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 10:30:41 +0200

    ADD Display the events

--- zooShell/src/main/java/doyenm/zooshell/commandLine/commandImpl/zoo/Evaluate.java
+++ zooShell/src/main/java/doyenm/zooshell/commandLine/commandImpl/zoo/Evaluate.java
@@ -5,12 +5,16 @@ import doyenm.zooshell.commandLine.general.Command;
 import doyenm.zooshell.commandLine.general.ReturnExec;
 import doyenm.zooshell.commandLine.general.TypeReturn;
 import doyenm.zooshell.context.EvaluationContext;
-import doyenm.zooshell.context.PaddockContext;
+import doyenm.zooshell.controller.eventhandling.Event;
 import doyenm.zooshell.controller.zoocontroller.EvaluationController;
 import doyenm.zooshell.model.Zoo;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import org.assertj.core.util.Arrays;
 
 /**
  *
@@ -18,7 +22,7 @@ import lombok.RequiredArgsConstructor;
  */
 @RequiredArgsConstructor
 public class Evaluate implements Command {
-    
+
     private final EvaluationController controller;
 
     @Override
@@ -28,7 +32,8 @@ public class Evaluate implements Command {
                 .map(controller)
                 .findFirst();
         if (optional.isPresent()) {
-            return new ReturnExec("OK", TypeReturn.SUCCESS, context.getZoo());
+            String events = formatEvents((EvaluationContext) optional.get());
+            return new ReturnExec(events, TypeReturn.SUCCESS, context.getZoo());
         } else {
             return new ReturnExec("ERROR", TypeReturn.ERROR);
         }
@@ -40,6 +45,22 @@ public class Evaluate implements Command {
             return Constants.EVALUATE.equals(cmd[0]);
         }
         return false;
+    }
+
+    private String formatEvents(EvaluationContext context) {
+        String result = "";
+        List<String> resultsList = new ArrayList<>();
+        context.getEvents()
+                .stream()
+                .forEach((Event event) ->{
+                   MessageFormat msg = new MessageFormat(event.getEventType().getMessage());
+                    resultsList.add(msg.format(new Object[]{event.getSubject().getName()}));
+//                            Arrays.array(event.getSubject().getName())));
+                });
+        for(String input : resultsList){
+            result += input + "\n";
+        }
+        return result;
     }
 
 }
