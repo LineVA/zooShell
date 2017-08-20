f4d04b31b440f087c086204f57e7d683caf3f60a
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 15:43:33 +0200

    ADD EventType Pregnancy

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
@@ -13,7 +13,8 @@ public enum EventType {
     DEATH_OF_HUNGER(EventCategory.DEATH, "The animal {0} is dead of starvation"),
     DIYING_OF_UNGER(EventCategory.DEATH, "The animal {0} is dying of starvation"),
     DIYING_OF_DROWN(EventCategory.DEATH, "The animal {0} is diying of drowning"),
-    BIRTH(EventCategory.BIRTH, "The animal {0} is born ; its mother is {1}");
+    BIRTH(EventCategory.BIRTH, "The animal {0} is born ; its mother is {1}"),
+    PREGNANCY(EventCategory.PREGNANCY, "The animal {0} is pregnant");
     
     @Getter
     private EventCategory category;
