d1e15c424a58392abda9a66abeb5913dd595466f
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 15:12:19 +0200

    ADD Create events related to birth

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/reproduction/ExecuteReproductionFunction.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/reproduction/ExecuteReproductionFunction.java
@@ -1,7 +1,12 @@
 package doyenm.zooshell.controller.animalcontroller.evaluation.reproduction;
 
 import doyenm.zooshell.context.AnimalEvaluationContext;
+import doyenm.zooshell.controller.eventhandling.Event;
+import doyenm.zooshell.controller.eventhandling.EventType;
+import doyenm.zooshell.model.Animal;
 import doyenm.zooshell.utils.UniformStatistics;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -36,14 +41,26 @@ public class ExecuteReproductionFunction
         }
         if (currentGestationDuration >= t.getGestationDuration()) {
             t.setCurrentGestationDuration(0);
-            return Stream.of(t)
+            AnimalEvaluationContext afterCalvingContext = Stream.of(t)
                     .map(calvingFunction)
                     .findFirst()
                     .get();
+            afterCalvingContext.getEvents().addAll(generateBirthEvents(afterCalvingContext));
+            return afterCalvingContext;
         } else {
             t.setCurrentGestationDuration(currentGestationDuration);
             return t;
         }
+    }
+    
+    private List<Event> generateBirthEvents(AnimalEvaluationContext context){
+        List<Event> events = new ArrayList<>();
+        context.getChildren()
+                .stream()
+                .forEach((Animal animal) -> {
+                    events.add(new Event(EventType.BIRTH, animal, context.getAnimal()));
+                });
+        return events;
     }
 
 }
