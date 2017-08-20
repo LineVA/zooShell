c69b2fbd9dbaaec88b93513960e58e3b254fdec8
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 17:44:13 +0200

    ADD Create an object to save the details of well being

---
+++ zooShell/src/main/java/doyenm/zooshell/model/WellBeing.java
@@ -0,0 +1,19 @@
+package doyenm.zooshell.model;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author doyenm
+ */
+@Getter
+public class WellBeing {
+    private double biomeWellBeing = 0.0;
+    private double dietsWellBeing = 0.0;
+    private double foodQuantityWellBeing = 0.0;
+    private double fastDaysWellBeing = 0.0;
+    private double territoryWellBeing = 0.0;
+    private double socialWellBeing = 0.0;
+    private double keepersTaskWellBeing = 0.0;
+    private double keepersTimeWellBeing = 0.0;
+}
