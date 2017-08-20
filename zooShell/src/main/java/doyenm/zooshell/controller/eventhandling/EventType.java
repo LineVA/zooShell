5a06da52f8ec47514ebf1e7a02cac3e31e0dca52
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 10:32:14 +0200

    FIX Typo

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
@@ -8,9 +8,9 @@ import lombok.Getter;
  */
 public enum EventType {
 
-    DEATH_BY_AGE("The animal {0} is dead of old age"),
-    DEATH_BY_DROWNING("The animal {0} is dead by drown"),
-    DEAT_BY_HUNGER("The animal {0} is dead of starvation");
+    DEATH_OF_AGE("The animal {0} is dead of old age"),
+    DEATH_OF_DROWN("The animal {0} is dead of drown"),
+    DEAT_OF_HUNGER("The animal {0} is dead of starvation");
     
     @Getter
     private final String message;
