04045e832a5c9e7453010bc0d189a3b937bb90c6
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 14:45:51 +0200

    ADD A a field "mother"
    Will be useful for births

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/Event.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/Event.java
@@ -1,6 +1,7 @@
 package doyenm.zooshell.controller.eventhandling;
 
 import doyenm.zooshell.model.Animal;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,8 +10,10 @@ import lombok.RequiredArgsConstructor;
  * @author doyenm
  */
 @RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 public class Event {
     private final EventType eventType;
     private final Animal subject;
+    private Animal mother;
 }
