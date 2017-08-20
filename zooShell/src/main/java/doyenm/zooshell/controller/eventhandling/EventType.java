ac4c892d754cd2bc074e594156bc0e737f612779
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-08-20 14:18:58 +0200

    ADD new events related to death

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/EventType.java
@@ -10,7 +10,9 @@ public enum EventType {
 
     DEATH_OF_AGE("The animal {0} is dead of old age"),
     DEATH_OF_DROWN("The animal {0} is dead of drown"),
-    DEATH_OF_HUNGER("The animal {0} is dead of starvation");
+    DEATH_OF_HUNGER("The animal {0} is dead of starvation"),
+    DIYING_OF_UNGER("The animal {0} is dying of starvation"),
+    DIYING_OF_DROWN("The animal {0} is diying of drowning");
     
     @Getter
     private final String message;
