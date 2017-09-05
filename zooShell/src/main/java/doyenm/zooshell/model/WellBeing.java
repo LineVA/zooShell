b5334013407b750780dad48957e33ad240bd8947
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-09-05 19:25:32 +0200

    UPDATE Use the new object of type WellBeing to compute the well-being

--- zooShell/src/main/java/doyenm/zooshell/model/WellBeing.java
+++ zooShell/src/main/java/doyenm/zooshell/model/WellBeing.java
@@ -1,12 +1,14 @@
 package doyenm.zooshell.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  *
  * @author doyenm
  */
 @Getter
+@Setter
 public class WellBeing {
     private double biomeWellBeing = 0.0;
     private double dietsWellBeing = 0.0;
@@ -16,4 +18,17 @@ public class WellBeing {
     private double socialWellBeing = 0.0;
     private double keepersTaskWellBeing = 0.0;
     private double keepersTimeWellBeing = 0.0;
+    
+    public double computeWellBeing(){
+        return (
+                getBiomeWellBeing()
+                + getDietsWellBeing()
+                + getFoodQuantityWellBeing()
+                + getFastDaysWellBeing()
+                + getTerritoryWellBeing()
+                + getSocialWellBeing()
+                + getKeepersTaskWellBeing()
+                + getKeepersTimeWellBeing()) 
+                / 8.0;
+    }
 }
