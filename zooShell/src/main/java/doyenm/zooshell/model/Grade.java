57efc537d0d195a74ee1bc9d63fbcdcecb652c03
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-09-06 10:45:21 +0200

    ADD Object to store the different parts of the grade of the zoo

---
+++ zooShell/src/main/java/doyenm/zooshell/model/Grade.java
@@ -0,0 +1,16 @@
+package doyenm.zooshell.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author doyenm
+ */
+@Getter
+@Setter
+public class Grade {
+    private double animalsGrade;
+    private double paddocksGrade;
+
+}
