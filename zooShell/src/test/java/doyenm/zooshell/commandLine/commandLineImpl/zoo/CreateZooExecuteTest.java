0c0e5f30836532dff69b6cd2ea40b99201250a2a
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-09-06 19:24:56 +0200

    ADD Tests about the execution of the creation of the zoo

--- zooShell/src/test/java/doyenm/zooshell/commandLine/commandLineImpl/zoo/CreateZooExecuteTest.java
+++ zooShell/src/test/java/doyenm/zooshell/commandLine/commandLineImpl/zoo/CreateZooExecuteTest.java
@@ -1,15 +1,17 @@
 package doyenm.zooshell.commandLine.commandLineImpl.zoo;
 
-import doyenm.zooshell.commandLine.commandImpl.animal.DetailAnimal;
-import doyenm.zooshell.commandLine.commandImpl.zoo.DetailZoo;
+import doyenm.zooshell.commandLine.commandImpl.paddock.CreatePaddock;
+import doyenm.zooshell.commandLine.commandImpl.zoo.CreateZoo;
 import doyenm.zooshell.commandLine.general.ReturnExec;
 import doyenm.zooshell.commandLine.general.TypeReturn;
-import doyenm.zooshell.context.AnimalDetailsContext;
-import doyenm.zooshell.context.ZooContext;
-import doyenm.zooshell.controller.animalcontroller.AnimalDetailsController;
-import doyenm.zooshell.controller.zoocontroller.ZooDetailsController;
+import doyenm.zooshell.context.PaddockCreationContext;
+import doyenm.zooshell.context.ZooCreationContext;
+import doyenm.zooshell.controller.paddockcontroller.PaddockCreationController;
+import doyenm.zooshell.controller.zoocontroller.ZooCreationController;
 import doyenm.zooshell.model.Zoo;
-import doyenm.zooshell.validator.AnimalDetailsValidator;
+import doyenm.zooshell.validator.PaddockCreationValidator;
+import doyenm.zooshell.validator.PaddockLocationValidator;
+import doyenm.zooshell.validator.ZooCreationValidator;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import static org.mockito.Matchers.any;
@@ -21,46 +23,55 @@ import org.mockito.stubbing.Answer;
  *
  * @author doyenm
  */
-public class DetailZooExecuteTest {
+public class CreateZooExecuteTest {
 
-    private ZooDetailsController givenController() {
-        ZooDetailsController controller = Mockito.mock(ZooDetailsController.class);
-        Mockito.when(controller.apply(any(ZooContext.class))).thenAnswer(new Answer<ZooContext>() {
+    private ZooCreationValidator givenCreationValidator(boolean value) {
+        ZooCreationValidator validator = Mockito.mock(ZooCreationValidator.class);
+        Mockito.doReturn(value).when(validator).test(any(ZooCreationContext.class));
+        return validator;
+    }
+
+    private ZooCreationController givenController() {
+        ZooCreationController controller = Mockito.mock(ZooCreationController.class);
+        Mockito.when(controller.apply(any(ZooCreationContext.class))).thenAnswer(new Answer<ZooCreationContext>() {
             @Override
-            public ZooContext answer(InvocationOnMock invocation) throws Throwable {
+            public ZooCreationContext answer(InvocationOnMock invocation) throws Throwable {
                 Object[] args = invocation.getArguments();
-                return (ZooContext) args[0];
+                return (ZooCreationContext) args[0];
             }
         });
         return controller;
     }
 
     @Test
-    public void shouldReturnAReturnExecWithTypeReturnToSuccessWhenTheCommandIsInSuccessAndTheLineContainsOneElement() {
+    public void shouldReturnAReturnExecWithTypeReturnToSuccessWhenTheCommandIsInSuccess() {
         // Given
-        ZooDetailsController controller = givenController();
-        DetailZoo command = new DetailZoo(controller);
-        String[] cmd = new String[1];
+        ZooCreationValidator creationValidator = givenCreationValidator(true);
+        ZooCreationController controller = givenController();
+        CreateZoo command = new CreateZoo(creationValidator, controller);
+        String[] cmd = new String[7];
         // When
         ReturnExec result = command.execute(cmd, Mockito.mock(Zoo.class));
         // Then
         Assertions.assertThat(result).isNotNull();
         Assertions.assertThat(result.getTypeReturn()).isEqualTo(TypeReturn.SUCCESS);
-        Assertions.assertThat(result.getMessage()).isNotNull();
+        Assertions.assertThat(result.getMessage()).isNotEmpty();
+        Assertions.assertThat(result.getZoo()).isNotNull();
     }
-
-     @Test
-    public void shouldReturnAReturnExecWithTypeReturnToSuccessWhenTheCommandIsInSuccessAndTheLineContainsTwoElements() {
+    
+    @Test
+    public void shouldReturnAReturnExecWithTypeReturnToSuccessWhenTheValidatorReturnsFalse() {
         // Given
-        ZooDetailsController controller = givenController();
-        DetailZoo command = new DetailZoo(controller);
-        String[] cmd = new String[2];
+        ZooCreationValidator creationValidator = givenCreationValidator(false);
+        ZooCreationController controller = givenController();
+        CreateZoo command = new CreateZoo(creationValidator, controller);
+        String[] cmd = new String[7];
         // When
         ReturnExec result = command.execute(cmd, Mockito.mock(Zoo.class));
         // Then
         Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result.getTypeReturn()).isEqualTo(TypeReturn.SUCCESS);
-        Assertions.assertThat(result.getMessage()).isNotNull();
+        Assertions.assertThat(result.getTypeReturn()).isEqualTo(TypeReturn.ERROR);
+        Assertions.assertThat(result.getMessage()).isNotEmpty();
     }
 
 }
