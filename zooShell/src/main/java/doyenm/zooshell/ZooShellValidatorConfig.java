76f73b097e22b3ae33590323411f8aaa24fdefe0
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-09-07 18:55:27 +0200

    UPDATE The properties of the validator are now defined into the properties file

--- zooShell/src/main/java/doyenm/zooshell/ZooShellValidatorConfig.java
+++ zooShell/src/main/java/doyenm/zooshell/ZooShellValidatorConfig.java
@@ -2,15 +2,22 @@ package doyenm.zooshell;
 
 import doyenm.zooshell.validator.SpecieDetailsValidator;
 import doyenm.zooshell.validator.ZooCreationValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 /**
  *
  * @author doyenm
  */
 @Configuration
+@PropertySource("classpath:/doyenm/zooshell/zooshell.properties")
 public class ZooShellValidatorConfig {
+
+    @Autowired
+    Environment environment;
 
     @Bean
     SpecieDetailsValidator specieDetailsValidator() {
@@ -19,6 +26,13 @@ public class ZooShellValidatorConfig {
 
     @Bean
     ZooCreationValidator zooCreationValidator() {
-        return new ZooCreationValidator();
+        return new ZooCreationValidator(
+                Integer.parseInt(environment.getProperty("zoo.width.min")),
+                Integer.parseInt(environment.getProperty("zoo.height.min")),
+                Integer.parseInt(environment.getProperty("zoo.horizon.min")),
+                Integer.parseInt(environment.getProperty("zoo.speed.min")),
+                Integer.parseInt(environment.getProperty("zoo.speed.max")),
+                Integer.parseInt(environment.getProperty("zoo.name.max_length"))
+        );
     }
 }
--- zooShell/src/main/java/doyenm/zooshell/validator/ZooCreationValidator.java
+++ zooShell/src/main/java/doyenm/zooshell/validator/ZooCreationValidator.java
@@ -4,30 +4,39 @@ import doyenm.zooshell.context.ZooCreationContext;
 import doyenm.zooshell.validator.predicates.IntegerValuePredicates;
 import doyenm.zooshell.validator.predicates.StringLengthPredicates;
 import java.util.function.Predicate;
+import lombok.RequiredArgsConstructor;
 
 /**
  *
  * @author doyenm
  */
+@RequiredArgsConstructor
 public class ZooCreationValidator implements Predicate<ZooCreationContext> {
 
     IntegerValuePredicates integerValuePredicates = new IntegerValuePredicates();
     StringLengthPredicates stringLengthPredicates = new StringLengthPredicates();
 
+    private final int minWidth;
+    private final int minHeight;
+    private final int minHorizon;
+    private final int minSpeed;
+    private final int maxSpeed;
+    private final int maxLengthName;
+
     @Override
     public boolean test(ZooCreationContext t) {
-    ZooCreationContext context = t;
+        ZooCreationContext context = t;
         context.convert();
         boolean result
-                = this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedWidth(),10);
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHeight(), 10);
+                = this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedWidth(), minWidth);
+        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHeight(), minHeight);
         int min = Math.min(context.getConvertedHeight(), context.getConvertedWidth());
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHorizon(), 2);
+        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHorizon(), minHorizon);
         result &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedHorizon(), min);
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedSpeed(), 2);
-        result &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedSpeed(), 13);
-        result &= this.stringLengthPredicates.mustBeLowerOrEqualsThan(context.getName(), 50);
-        return result;    
+        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedSpeed(), minSpeed);
+        result &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedSpeed(), maxSpeed);
+        result &= this.stringLengthPredicates.mustBeLowerOrEqualsThan(context.getName(), maxLengthName);
+        return result;
     }
 }
 
