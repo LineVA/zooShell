3063528b3b0c4ca9f415155f0486054c861a84f2
Marine Doyen-Le Boulaire <marine.doyen-le-boulaire@ensimag.grenoble-inp.fr>
2017-10-08 19:05:41 +0200

    ADD Use of the new class in the constructor of CharacterAttributes

--- zooShell/src/main/java/doyenm/zooshell/model/utils/CharacterAttributesConstructor.java
+++ zooShell/src/main/java/doyenm/zooshell/model/utils/CharacterAttributesConstructor.java
@@ -1,12 +1,10 @@
 package doyenm.zooshell.model.utils;
 
 import doyenm.zooshell.model.CharacterAttributes;
-import doyenm.zooshell.model.Diet;
 import doyenm.zooshell.model.Sex;
 import doyenm.zooshell.model.SizeAttributes;
 import doyenm.zooshell.model.Specie;
 import doyenm.zooshell.utils.GaussianStatistics;
-import java.util.stream.Stream;
 
 /**
  *
@@ -15,6 +13,7 @@ import java.util.stream.Stream;
 public class CharacterAttributesConstructor {
 
     private final GaussianStatistics gaussianStatistics = new GaussianStatistics();
+    private final CohabitationFactorHandler cohabitationFactorHandler = new CohabitationFactorHandler();
 
     public CharacterAttributes build(Specie specie, SizeAttributes sizeAttributes, Sex sex) {
         double tmpAgressivity = gaussianStatistics.nextDouble();
@@ -23,37 +22,8 @@ public class CharacterAttributesConstructor {
         double tmpIntelligence = gaussianStatistics.nextDouble();
         double tmpCuriosity = gaussianStatistics.nextDouble();
         double tmpMeticulousness = gaussianStatistics.nextDouble();
-        double cohabitationFactor = determineCohabitationFactor(tmpAgressivity, specie, sizeAttributes, sex);
+        double cohabitationFactor = cohabitationFactorHandler.compute(tmpAgressivity, specie, sizeAttributes, sex);
         return new CharacterAttributes(tmpAgressivity, tmpBravery, cohabitationFactor,
                 tmpCuriosity, tmpGourmandise, tmpIntelligence, tmpMeticulousness);
     }
-
-    public double determineCohabitationFactor(double agressivity, Specie specie, SizeAttributes sizeAttributes, Sex sex) {
-        double cohabitation = 0.0;
-        double weight = sizeAttributes.getWeigthGivenSex(sex);
-
-        // Cohabitation = (
-        //    weight/ 1 tonne
-        //    + 1 if carnivore
-        //    + agressivity
-        //        + specie_agresivity)
-        //    / 4
-        return Stream.of(cohabitation)
-                .map((Double t) -> {
-                    t += weight >= 1000.0 ? 1.0 : weight / 1000.0;
-                    return t;
-                })
-                .map((Double t) -> {
-                    if (specie.getDiets().getDiets().contains(Diet.CARNIVOROUS)) {
-                        t += 1.0;
-                    }
-                    return t;
-                })
-                .map((Double t) -> t += agressivity)
-                .map((Double t) -> t += specie.getCharacterAttributes().getAgressivity())
-                .map((Double t) -> t / 4.0)
-                .findFirst()
-                .get();
-    }
-
 }
