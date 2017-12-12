8d95d949f1bbe8a299ce1829b48196ee54e34fb0
Marine <doyenm@carrotCake>
2017-12-12 22:11:35 +0100

    CLEAN Import

--- zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockEvaluationController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockEvaluationController.java
@@ -1,16 +1,8 @@
 package doyenm.zooshell.controller.paddockcontroller;
 
-import doyenm.zooshell.context.AnimalEvaluationContext;
 import doyenm.zooshell.context.EvaluationContext;
 import doyenm.zooshell.context.PaddockEvaluationContext;
-import doyenm.zooshell.controller.animalcontroller.evaluation.AnimalAgeEvaluationController;
-import doyenm.zooshell.controller.animalcontroller.evaluation.AnimalDeathEvaluationController;
-import doyenm.zooshell.controller.animalcontroller.evaluation.AnimalReproductionEvaluationController;
-import doyenm.zooshell.controller.animalcontroller.evaluation.AnimalWellBeingController;
-import doyenm.zooshell.model.Animal;
 import doyenm.zooshell.model.Paddock;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
