de8f7da3d4c16be63c99d1bde594b67609276fc5
Marine <doyenm@carrotCake>
2017-12-17 20:52:17 +0100

    CLEAN

--- zooShell/src/test/java/doyenm/zooshell/validator/KeeperCreationValidatorTestTest.java
+++ zooShell/src/test/java/doyenm/zooshell/validator/KeeperCreationValidatorTestTest.java
@@ -5,18 +5,12 @@ import doyenm.zooshell.model.AnimalKeeper;
 import doyenm.zooshell.validator.name.NameDto;
 import doyenm.zooshell.validator.name.NameValidator;
 import doyenm.zooshell.validator.predicates.KeepersNumberPredicate;
-import doyenm.zooshell.validator.predicates.StringLengthPredicates;
-import doyenm.zooshell.validator.predicates.UniquenessNamesBiPredicates;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.lang.RandomStringUtils;
-import org.apache.commons.lang.math.RandomUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anySet;
-import static org.mockito.Matchers.anyString;
 import org.mockito.Mockito;
 
 /**
