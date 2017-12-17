83f2c01e0118b725fe12fa9bb19dd50e588d2df2
Marine <doyenm@carrotCake>
2017-12-17 11:42:09 +0100

    ADD Enum with all the events related to a paddock

---
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/paddock/PaddockEventType.java
@@ -0,0 +1,24 @@
+package doyenm.zooshell.controller.eventhandling.paddock;
+
+import doyenm.zooshell.controller.eventhandling.EventCategory;
+import lombok.Getter;
+
+/**
+ *
+ * @author doyenm
+ */
+public enum PaddockEventType {
+
+   CHANGE_STATE(EventCategory.BINARY, "The state of the paddock {0} has changed : its state is now \' {1} \' ");
+
+    @Getter
+    private EventCategory category;
+    
+    @Getter
+    private final String message;
+
+    private PaddockEventType(EventCategory category, String message) {
+        this.category = category;
+        this.message = message;
+    }
+}
