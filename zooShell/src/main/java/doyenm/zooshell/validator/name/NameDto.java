d6232d76a09000120800f964a71f954fffc83f1e
Marine <doyenm@carrotCake>
2017-12-17 18:57:18 +0100

    ADD Validator in charge of testing the validity of each new name

---
+++ zooShell/src/main/java/doyenm/zooshell/validator/name/NameDto.java
@@ -0,0 +1,17 @@
+package doyenm.zooshell.validator.name;
+
+import java.util.Collection;
+import java.util.Set;
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ *
+ * @author doyenm
+ */
+@Getter
+@Builder
+public class NameDto {
+    private final String testing;
+    private final Set<String> exitingNames;
+}
---
+++ zooShell/src/main/java/doyenm/zooshell/validator/name/NameValidator.java
@@ -0,0 +1,24 @@
+package doyenm.zooshell.validator.name;
+
+import doyenm.zooshell.validator.predicates.StringLengthPredicates;
+import doyenm.zooshell.validator.predicates.UniquenessNamesBiPredicates;
+import java.util.function.Predicate;
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author doyenm
+ */
+@RequiredArgsConstructor
+public class NameValidator implements Predicate<NameDto> {
+
+    private final StringLengthPredicates stringLengthPredicates;
+    private final UniquenessNamesBiPredicates uniquenessNamesBiPredicates;
+    private final int maxLengthName;
+
+    @Override
+    public boolean test(NameDto t) {
+        return this.stringLengthPredicates.mustBeLowerOrEqualsThan(t.getTesting(), maxLengthName)
+                & this.uniquenessNamesBiPredicates.test(t.getTesting().toUpperCase(), t.getExitingNames());
+    }
+}
