340250db372efecd494ff63b694ab91ba2220c99
Marine <doyenm@carrotCake>
2017-12-17 22:21:37 +0100

    ADD Controller to create an handyman

---
+++ zooShell/src/main/java/doyenm/zooshell/controller/handymancontroller/CreationController.java
@@ -0,0 +1,24 @@
+package doyenm.zooshell.controller.handymancontroller;
+
+import doyenm.zooshell.context.HandymanCreationContext;
+import doyenm.zooshell.model.Handyman;
+import java.util.function.Function;
+
+/**
+ *
+ * @author doyenm
+ */
+public class CreationController
+        implements Function<HandymanCreationContext, HandymanCreationContext>{
+
+    @Override
+    public HandymanCreationContext apply(HandymanCreationContext t) {
+        Handyman handyman = Handyman.builder()
+                .name(t.getName())
+                .age(0)
+                .build();
+        t.getZoo().getHandymen().put(t.getName(), handyman);
+        return t;
+    }
+    
+}
---
+++ zooShell/src/test/java/doyenm/zooshell/controller/handymancontroller/HandymanCreationController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doyenm.zooshell.controller.handymancontroller;
+
+/**
+ *
+ * @author doyenm
+ */
+class HandymanCreationController {
+    
+}
