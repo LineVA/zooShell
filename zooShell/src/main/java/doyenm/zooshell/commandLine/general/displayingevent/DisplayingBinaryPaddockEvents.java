53828c35f7eb134416e64ab2e46e75338cc5a358
Marine <doyenm@carrotCake>
2017-12-17 11:55:07 +0100

    ADD New class in charge of diplaying the paddock events

--- zooShell/src/main/java/doyenm/zooshell/commandLine/general/displayingevent/DisplayingBinaryPaddockEvents.java
+++ zooShell/src/main/java/doyenm/zooshell/commandLine/general/displayingevent/DisplayingBinaryPaddockEvents.java
@@ -1,29 +1,29 @@
 package doyenm.zooshell.commandLine.general.displayingevent;
 
 import doyenm.zooshell.controller.eventhandling.Event;
-import doyenm.zooshell.controller.eventhandling.animal.AnimalEvent;
 import doyenm.zooshell.controller.eventhandling.EventCategory;
 import doyenm.zooshell.controller.eventhandling.EventSubject;
+import doyenm.zooshell.controller.eventhandling.paddock.PaddockEvent;
 import java.text.MessageFormat;
 
 /**
  *
  * @author doyenm
  */
-public class DisplayingBinaryAnimalEvents implements DisplayingEvents {
+public class DisplayingBinaryPaddockEvents implements DisplayingEvents {
 
     @Override
     public boolean canFormat(Event event) {
-        return EventSubject.ANIMAL_EVENT == event.getEventSubject()
+        return EventSubject.PADDOCK_EVENT == event.getEventSubject()
                 && EventCategory.BINARY == event.getEventCategory();
     }
 
     @Override
     public String format(Event event) {
-        AnimalEvent animalEvent = (AnimalEvent) event;
+        PaddockEvent paddockEvent = (PaddockEvent) event;
 
-        return MessageFormat.format(animalEvent.getEventType().getMessage(),
-                animalEvent.getSubject().getName(),
-                animalEvent.getActor().getName());
+        return MessageFormat.format(paddockEvent.getEventType().getMessage(),
+                paddockEvent.getSubject().getName(),
+                paddockEvent.getActor().toString());
     }
 }
