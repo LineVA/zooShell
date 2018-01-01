1ca2ebbcc690d7664e01821b908d716f6be03acb
Marine <doyenm@carrotCake>
2018-01-01 15:37:15 +0100

    UPDATE Getter and Setter

--- zooShell/src/main/java/doyenm/zooshell/context/HandymanUpdateOccupationsContext.java
+++ zooShell/src/main/java/doyenm/zooshell/context/HandymanUpdateOccupationsContext.java
@@ -3,12 +3,16 @@ package doyenm.zooshell.context;
 import doyenm.zooshell.model.Handyman;
 import doyenm.zooshell.model.Paddock;
 import doyenm.zooshell.model.Zoo;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 /**
  *
  * @author doyenm
  */
+@Setter
+@Getter
 @RequiredArgsConstructor
 public class HandymanUpdateOccupationsContext {
     private final Zoo zoo;
