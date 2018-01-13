f8518e9bcae1771854aeca9116fb73d0bd3a5f77
Marine <doyenm@carrotCake>
2018-01-13 16:50:09 +0100

    ADD Management of penalities when evaluating

--- zooShell/src/main/java/doyenm/zooshell/controller/EvaluationController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/EvaluationController.java
@@ -22,6 +22,7 @@ public class EvaluationController implements Function<EvaluationContext, Evaluat
     private final PaddockEvaluationController paddockEvaluationController;
     private final HandymanEvaluationController handymanEvaluationController;
     private final ZooEvaluationController zooEvaluationController;
+    private final PenaltiesEvaluationController penaltiesEvaluationController;
 
     @Override
     public EvaluationContext apply(EvaluationContext t) {
@@ -32,6 +33,7 @@ public class EvaluationController implements Function<EvaluationContext, Evaluat
                 .map(animalEvaluationController)
                 .map(keeperEvaluationController)
                 .map(handymanEvaluationController)
+                .map(penaltiesEvaluationController)
                 .map((EvaluationContext t1) -> {
                     t1.getZoo().setGradeObj(t1.getGradeObj());
                     t1.getZoo().setGrade(t1.getZoo().getGradeObj().computeGrade());
@@ -42,5 +44,4 @@ public class EvaluationController implements Function<EvaluationContext, Evaluat
         context.updateAnimals();
         return context;
     }
-
 }
---
+++ zooShell/src/main/java/doyenm/zooshell/controller/PenaltiesEvaluationController.java
@@ -0,0 +1,25 @@
+package doyenm.zooshell.controller;
+
+import doyenm.zooshell.context.EvaluationContext;
+import java.util.function.Function;
+
+/**
+ *
+ * @author doyenm
+ */
+public class PenaltiesEvaluationController
+        implements Function<EvaluationContext, EvaluationContext> {
+
+    @Override
+    public EvaluationContext apply(EvaluationContext t) {
+        t.getZoo().getPenalties()
+                .stream()
+                .forEach( penalty -> {
+                   t.getGradeObj().setPenaltiesGrade(penalty.getValue() + t.getGradeObj().getPenaltiesGrade());
+                   penalty.setRemainingTurns(penalty.getRemainingTurns() - 1);
+                });
+        t.getZoo().getPenalties().removeIf(penalty -> penalty.getRemainingTurns() == 0);
+        return t;
+    }
+
+}
