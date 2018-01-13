2dea80886a38b2b05247340c96f519ffd52a8e28
Marine <doyenm@carrotCake>
2018-01-13 15:43:44 +0100

    ADD Notion of penalty

---
+++ zooShell/src/main/java/doyenm/zooshell/model/Penalty.java
@@ -0,0 +1,26 @@
+package doyenm.zooshell.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author doyenm
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Penalty {
+    private double value;
+    private int remainingTurns;
+}
