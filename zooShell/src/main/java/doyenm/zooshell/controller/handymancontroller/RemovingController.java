927defe3805f428da77685eb5b2f106f66aef2ac
Marine <doyenm@carrotCake>
2018-01-14 10:59:18 +0100

    ADD Controlleur to remove an handyman

---
+++ zooShell/src/main/java/doyenm/zooshell/controller/handymancontroller/RemovingController.java
@@ -0,0 +1,19 @@
+package doyenm.zooshell.controller.handymancontroller;
+
+import doyenm.zooshell.context.HandymanContext;
+import java.util.function.Function;
+
+/**
+ *
+ * @author doyenm
+ */
+public class RemovingController
+        implements Function<HandymanContext, HandymanContext>{
+
+    @Override
+    public HandymanContext apply(HandymanContext t) {
+        t.getZoo().getHandymen().remove(t.getHandymanName().toUpperCase());
+        return t;
+    }
+    
+}
