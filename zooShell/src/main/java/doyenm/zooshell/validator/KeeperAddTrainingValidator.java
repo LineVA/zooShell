114049e147a4456b39c72ad58051d89859ce66af
Marine <doyenm@carrotCake>
2018-01-20 23:05:18 +0100

    ADD Function to find a family

---
+++ zooShell/src/main/java/doyenm/zooshell/validator/KeeperAddTrainingValidator.java
@@ -0,0 +1,30 @@
+package doyenm.zooshell.validator;
+
+import doyenm.zooshell.context.KeeperRenameContext;
+import doyenm.zooshell.validator.name.NameDto;
+import doyenm.zooshell.validator.name.NameValidator;
+import java.util.function.Predicate;
+import lombok.RequiredArgsConstructor;
+
+/**
+ *
+ * @author doyenm
+ */
+@RequiredArgsConstructor
+public class KeeperRenameValidator implements Predicate<KeeperRenameContext> {
+
+    private final NameValidator nameValidator;
+    private final FindKeeper findKeeper;
+
+    @Override
+    public boolean test(KeeperRenameContext t) {
+        boolean result;
+        result = nameValidator.test(NameDto.builder()
+                .testing(t.getNewKeeperName())
+                .existingNames(t.getKeepers().keySet())
+                .build());
+        t.setConvertedKeeper(findKeeper.find(t.getZoo(), t.getKeeper()));
+        return result & t.getConvertedKeeper() != null;
+    }
+
+}
