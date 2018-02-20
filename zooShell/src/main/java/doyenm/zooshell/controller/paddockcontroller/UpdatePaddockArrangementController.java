66e0290cbeb7483447a9552f1ab8498c1323e1a4
Marine <doyenm@carrotCake>
2018-02-20 22:44:13 +0100

    ADD Controller to add an arrangement

--- zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/UpdatePaddockArrangementController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/UpdatePaddockArrangementController.java
@@ -1,18 +1,21 @@
 package doyenm.zooshell.controller.paddockcontroller;
 
-import doyenm.zooshell.context.UpdatePaddockTypeContext;
+import doyenm.zooshell.context.UpdatePaddockArrangementContext;
 import java.util.function.Function;
 
 /**
  *
  * @author doyenm
  */
-public class UpdatePaddockTypeController
-        implements Function<UpdatePaddockTypeContext, UpdatePaddockTypeContext> {
+public class UpdatePaddockArrangementController
+        implements Function<UpdatePaddockArrangementContext, UpdatePaddockArrangementContext> {
 
     @Override
-    public UpdatePaddockTypeContext apply(UpdatePaddockTypeContext t) {
-        t.build();
+    public UpdatePaddockArrangementContext apply(UpdatePaddockArrangementContext t) {
+        if (!t.getConvertedPaddock().getArrangements().contains(t.getConvertedArrangement())) {
+            t.getConvertedPaddock().getArrangements().add(t.getConvertedArrangement());
+            t.getZoo().getPaddocks().replace(t.getPaddock(), t.getConvertedPaddock());
+        }
         return t;
     }
 
