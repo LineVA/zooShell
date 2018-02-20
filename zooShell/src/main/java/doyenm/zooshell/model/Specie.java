a80cc03c9249ba1d84f31a704ead39bd2a2a36f9
Marine <doyenm@carrotCake>
2018-02-20 23:04:54 +0100

    ADD A specie has associated arrangements

--- zooShell/src/main/java/doyenm/zooshell/model/Specie.java
+++ zooShell/src/main/java/doyenm/zooshell/model/Specie.java
@@ -38,6 +38,7 @@ public class Specie {
     private LifespanAttributes lifespanAttributes;
     private BiomesSpecie biomes;
     private DietsSpecie diets;
+    private PaddockArrangementSpecie arrangements;
     private FoodAttributes foodAttributes;
     private SocialAttributes socialAttributes;
     private TerritoryAttributes territoryAttributes;
@@ -53,10 +54,12 @@ public class Specie {
 //    int breedingProgramme;
 //    Tags tags;
 
-    public Specie(Names names, Uicn uicn, BiomesSpecie biomes, DietsSpecie diets) {
+    public Specie(Names names, Uicn uicn, BiomesSpecie biomes, DietsSpecie diets,
+            PaddockArrangementSpecie arrangements) {
         this.names = names;
         this.uicn = uicn;
         this.biomes = biomes;
         this.diets = diets;
+        this.arrangements = arrangements;
     }
 }
