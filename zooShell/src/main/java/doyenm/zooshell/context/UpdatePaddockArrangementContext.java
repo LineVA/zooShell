9ff2fea423e4de7934e9476aad884eb9eeb1f4cc
Marine <doyenm@carrotCake>
2018-02-20 22:36:42 +0100

    ADD Context to update the arrangements

---
+++ zooShell/src/main/java/doyenm/zooshell/context/UpdatePaddockArrangementContext.java
@@ -0,0 +1,30 @@
+package doyenm.zooshell.context;
+
+import doyenm.zooshell.model.Paddock;
+import doyenm.zooshell.model.PaddockArrangement;
+import doyenm.zooshell.model.Zoo;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author doyenm
+ */
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class UpdatePaddockArrangementContext {
+
+    private final Zoo zoo;
+    private final String paddock;
+    private final String arrangement;
+
+    private Paddock convertedPaddock;
+    private PaddockArrangement convertedArrangement;
+
+//    public void build() {
+//        this.getConvertedPaddock().setType(this.getConvertedType());
+//        this.getZoo().getPaddocks().replace(this.getPaddock(), this.getConvertedPaddock());
+//    }
+}
