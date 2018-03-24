ae676d1c679631a22981baf1a41adeda2d352593
Marine <doyenm@carrotCake>
2018-03-24 21:20:17 +0100

    SONAR Enclosing if

--- zooShell/src/main/java/doyenm/zooshell/commandline/commandimpl/handyman/CreateHandyman.java
+++ zooShell/src/main/java/doyenm/zooshell/commandline/commandimpl/handyman/CreateHandyman.java
@@ -8,13 +8,14 @@ import doyenm.zooshell.controller.handymancontroller.CreationController;
 import doyenm.zooshell.model.Zoo;
 import doyenm.zooshell.utils.Constants;
 import doyenm.zooshell.validator.HandymanCreationValidator;
+
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.Stream;
+
 import lombok.RequiredArgsConstructor;
 
 /**
- *
  * @author doyenm
  */
 @RequiredArgsConstructor
@@ -25,34 +26,24 @@ public class CreateHandyman implements Command {
 
     @Override
     public ReturnExec execute(String[] cmd, Zoo zoo) {
-        try {
             HandymanCreationContext context = new HandymanCreationContext(zoo,
                     cmd[2]);
             Optional<HandymanCreationContext> optional = Stream.of(context)
                     .filter(validator)
                     .map(controller)
                     .findFirst();
-           if (optional.isPresent()) {
-            return new ReturnExec("HANDYMAN_CREATION_SUCCESS", TypeReturn.SUCCESS, context.getZoo());
-        } else {
-            return new ReturnExec("ERROR", TypeReturn.ERROR, context.getZoo());
-        }
-        } catch (java.util.NoSuchElementException ex) {
-            ex.printStackTrace();
-            return new ReturnExec("ERROR", TypeReturn.ERROR);
-        }
+            if (optional.isPresent()) {
+                return new ReturnExec("HANDYMAN_CREATION_SUCCESS", TypeReturn.SUCCESS, context.getZoo());
+            } else {
+                return new ReturnExec("ERROR", TypeReturn.ERROR, context.getZoo());
+            }
     }
 
     @Override
     public boolean canExecute(String[] cmd) {
-        if (cmd.length == 3) {
-            if (Arrays.asList(Constants.HANDYMAN_OR_HD).contains(cmd[0])) {
-                if (Constants.CREATE.equalsIgnoreCase(cmd[1])) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return cmd.length == 3
+                && Arrays.asList(Constants.HANDYMAN_OR_HD).contains(cmd[0])
+                && Constants.CREATE.equalsIgnoreCase(cmd[1]);
     }
 
 }
