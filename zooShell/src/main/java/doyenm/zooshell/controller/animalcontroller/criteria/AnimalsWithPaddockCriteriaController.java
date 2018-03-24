7638660e274e1748b10a3732e5a2d5d76cf7a81d
Marine <doyenm@carrotCake>
2018-03-24 18:50:56 +0100

    SONAR Get without checking if it exists

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/criteria/AnimalsWithPaddockCriteriaController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/criteria/AnimalsWithPaddockCriteriaController.java
@@ -2,21 +2,24 @@ package doyenm.zooshell.controller.animalcontroller.criteria;
 
 import doyenm.zooshell.context.AnimalsWithCriteriaContext;
 import doyenm.zooshell.model.Paddock;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 
 /**
- *
  * @author doyenm
  */
+@Slf4j
 public class AnimalsWithPaddockCriteriaController
         implements Function<AnimalsWithCriteriaContext, AnimalsWithCriteriaContext> {
 
     @Override
     public AnimalsWithCriteriaContext apply(AnimalsWithCriteriaContext t) {
-        return Arrays.asList(t)
+        Optional<AnimalsWithCriteriaContext> optional = Arrays.asList(t)
                 .stream()
                 .map(t1 -> {
                     for (Map.Entry<String, Paddock> entry : t1.getConvertedPaddocks().entrySet()) {
@@ -31,8 +34,13 @@ public class AnimalsWithPaddockCriteriaController
                     t1.setPaddockExpression(BuildingExpression.rebuildBooleanExpression(t1.getPaddockExpressionList()));
                     return t1;
                 })
-                .findFirst()
-                .get();
+                .findFirst();
+        if (optional.isPresent()) {
+            return optional.get();
+        } else {
+            log.info("Fatal Error ; no value returned after examining paddock criteria on animal {}", t.getAnimal().getName());
+            return t;
+        }
     }
 
 }
