b483baea9b271c231781f362e37a15b586ee19ae
Marine <doyenm@carrotCake>
2018-03-24 16:58:32 +0100

    Sonar

--- zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/wellbeing/AnimalDietsEvaluationController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/animalcontroller/evaluation/wellbeing/AnimalDietsEvaluationController.java
@@ -2,13 +2,13 @@ package doyenm.zooshell.controller.animalcontroller.evaluation.wellbeing;
 
 import doyenm.zooshell.context.AnimalEvaluationContext;
 import doyenm.zooshell.model.Diet;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
- *
  * @author doyenm
  */
 public class AnimalDietsEvaluationController
@@ -23,8 +23,8 @@ public class AnimalDietsEvaluationController
             List<Diet> diets = new ArrayList<>();
             List<Diet> animalDiets = context.getAnimal().getDiets();
             List<Diet> specieDiets = context.getAnimal().getSpecie().getDiets().getDiets();
-            diets = addAllIfAbsent(diets, animalDiets);
-            diets = addAllIfAbsent(diets, specieDiets);
+            addAllIfAbsent(diets, animalDiets);
+            addAllIfAbsent(diets, specieDiets);
             int sum = 0;
             for (Diet animalDiet : animalDiets) {
                 if (specieDiets.contains(animalDiet)) {
@@ -32,18 +32,17 @@ public class AnimalDietsEvaluationController
                 }
             }
             context.getWellBeingObj().setDietsWellBeing(context.getBase() * (double) sum / ((double) diets.size()));
-//            context.setDietsWellBeing(context.getBase() * (double) sum / ((double) diets.size()));
         }
         return context;
     }
 
     private boolean isNullOrEmpty(AnimalEvaluationContext context) {
         if (context.getAnimal().getSpecie().getDiets().getDiets() == null
-                | context.getAnimal().getDiets() == null) {
+                || context.getAnimal().getDiets() == null) {
             return true;
         } else {
             return context.getAnimal().getDiets().isEmpty()
-                    | context.getAnimal().getSpecie().getDiets().getDiets().isEmpty();
+                    || context.getAnimal().getSpecie().getDiets().getDiets().isEmpty();
         }
     }
 
