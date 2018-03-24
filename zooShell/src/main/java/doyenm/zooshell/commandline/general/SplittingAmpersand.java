d933bad6fec674309f3e5ada4c9238c38f9acddf
Marine <doyenm@carrotCake>
2018-03-24 22:15:44 +0100

    SONAR Unused variable

--- zooShell/src/main/java/doyenm/zooshell/commandline/general/SplittingAmpersand.java
+++ zooShell/src/main/java/doyenm/zooshell/commandline/general/SplittingAmpersand.java
@@ -9,12 +9,11 @@ import java.util.Set;
  */
 public class SplittingAmpersand {
 
+    private SplittingAmpersand(){}
+
     public static Set<String> split(String str) {
         String[] aux = str.split("&+");
         Set<String> auxList = new HashSet<>();
-        String[] aux2;
-//        for(int j=0 ; j<aux.length ; j++){
-//           aux2 = SplitDoubleQuotes.split(aux[j]);
         for (int i = 0; i < aux.length; i++) {
             if (aux[i].substring(0, 1).equals("\"") && aux[i].endsWith("\"")) {
                 auxList.add(aux[i].substring(1, aux[i].length() - 1));
