5b3a344a4a799610bc586e03bbba2f4909f8d3d4
Marine <doyenm@carrotCake>
2018-03-24 21:30:31 +0100

    SONAR Unused import

--- zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/zoo/ZooEvent.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/eventhandling/zoo/ZooEvent.java
@@ -3,8 +3,6 @@ package doyenm.zooshell.controller.eventhandling.zoo;
 import doyenm.zooshell.controller.eventhandling.Event;
 import doyenm.zooshell.controller.eventhandling.EventCategory;
 import doyenm.zooshell.controller.eventhandling.EventSubject;
-import doyenm.zooshell.model.AnimalKeeper;
-import doyenm.zooshell.model.Family;
 import lombok.Builder;
 import lombok.Getter;
 
