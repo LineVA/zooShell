07edd1cf86fe7fa54a48f9bc10b7a17ae1ff5ea1
Marine <doyenm@carrotCake>
2018-03-24 21:36:43 +0100

    SONAR Make a field static

--- zooShell/src/main/java/doyenm/zooshell/utils/Utils.java
+++ zooShell/src/main/java/doyenm/zooshell/utils/Utils.java
@@ -1,18 +1,17 @@
 package doyenm.zooshell.utils;
 
 /**
- *
  * @author doyenm
  */
 public class Utils {
-    
-    private final int NUMBER_OF_MONTHS_PER_YEAR = 12;
-    
-    public int getNumbersOfYearsFromAge(int age){
+
+    private static final int NUMBER_OF_MONTHS_PER_YEAR = 12;
+
+    public int getNumbersOfYearsFromAge(int age) {
         return age / this.NUMBER_OF_MONTHS_PER_YEAR;
     }
-    
-      public int getNumbersOfMonthsFromAge(int age){
+
+    public int getNumbersOfMonthsFromAge(int age) {
         return age % this.NUMBER_OF_MONTHS_PER_YEAR;
     }
 }
