d9318f1fab9ff54875cd31043cd54f942a668785
Marine <doyenm@carrotCake>
2018-03-24 21:55:00 +0100

    SONAR Correct use of static field

--- zooShell/src/main/java/doyenm/zooshell/utils/Utils.java
+++ zooShell/src/main/java/doyenm/zooshell/utils/Utils.java
@@ -8,10 +8,10 @@ public class Utils {
     private static final int NUMBER_OF_MONTHS_PER_YEAR = 12;
 
     public int getNumbersOfYearsFromAge(int age) {
-        return age / this.NUMBER_OF_MONTHS_PER_YEAR;
+        return age / NUMBER_OF_MONTHS_PER_YEAR;
     }
 
     public int getNumbersOfMonthsFromAge(int age) {
-        return age % this.NUMBER_OF_MONTHS_PER_YEAR;
+        return age % NUMBER_OF_MONTHS_PER_YEAR;
     }
 }
