092254fd894b86e63012aee261c15525f7741567
Marine <doyenm@carrotCake>
2018-03-24 19:42:02 +0100

    SONAR Unused import

--- zooShell/src/main/java/doyenm/zooshell/model/Biome.java
+++ zooShell/src/main/java/doyenm/zooshell/model/Biome.java
@@ -1,11 +1,11 @@
 package doyenm.zooshell.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 /**
  *
