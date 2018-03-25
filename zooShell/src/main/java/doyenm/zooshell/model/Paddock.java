c800cc215dae67f6e96989c707c983237a37a60d
Marine <doyenm@carrotCake>
2018-03-25 11:25:37 +0200

    SONAR Unused class

--- zooShell/src/main/java/doyenm/zooshell/model/Paddock.java
+++ zooShell/src/main/java/doyenm/zooshell/model/Paddock.java
@@ -1,15 +1,11 @@
 package doyenm.zooshell.model;
 
-import doyenm.zooshell.launch.options.Option;
-import java.util.List;
+import lombok.*;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.util.List;
 
 /**
  *
@@ -24,7 +20,6 @@ import lombok.Setter;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Paddock {
 
-    private Option option;
     /**
      * The name of the paddock
      */
