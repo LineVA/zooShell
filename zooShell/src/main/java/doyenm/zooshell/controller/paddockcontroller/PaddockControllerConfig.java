0a4d4499b0fcfcdf0543f7789225f2b0dd5519bd
Marine <doyenm@carrotCake>
2018-03-25 11:22:19 +0200

    SONAR Commented code

--- zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockControllerConfig.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/paddockcontroller/PaddockControllerConfig.java
@@ -122,7 +122,6 @@ public class PaddockControllerConfig {
             @Override
             public Double apply(LightZooDto dto) {
                 return 0.001 * (1 + dto.getSpeed() * dto.getAnimalsOfThePaddock());
-//                return 0.8;
             }
         };
     }
