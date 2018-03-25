b0f90d65a30dc772729fd88d3f608a7c4aa2ba5a
Marine <doyenm@carrotCake>
2018-03-25 11:54:02 +0200

    SONAR Unused variable

--- zooShell/src/main/java/doyenm/zooshell/controller/keepercontroller/KeeperEvaluationTrainingController.java
+++ zooShell/src/main/java/doyenm/zooshell/controller/keepercontroller/KeeperEvaluationTrainingController.java
@@ -22,10 +22,7 @@ public class KeeperEvaluationTrainingController
         if (training != null) {
             training.setRemainingTurns(training.getRemainingTurns() - 1);
             if (training.getRemainingTurns() <= 0) {
-//                keeper.getFamilyEvaluations().replace(
-//                        training.getFamilySubject(),
-//                        keeper.getFamilyEvaluations().get(training.getFamilySubject()) + training.getBonus());
-                keeper = updateEvaluation(keeper);
+                updateEvaluation(keeper);
                 keeper.setTraining(null);
                 t.getEvents().add(new KeeperEvent(KeeperEventType.END_OF_TRAINING, keeper));
             }
