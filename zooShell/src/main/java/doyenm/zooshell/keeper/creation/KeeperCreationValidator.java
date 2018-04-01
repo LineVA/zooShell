16123c9d827cdc791d1d0bf2cda82992ec5082a7
Marine <doyenm@carrotCake>
2018-04-01 12:50:18 +0200

    UPDATE Use of stream

--- zooShell/src/main/java/doyenm/zooshell/keeper/creation/KeeperCreationValidator.java
+++ zooShell/src/main/java/doyenm/zooshell/keeper/creation/KeeperCreationValidator.java
@@ -4,27 +4,30 @@ import doyenm.zooshell.common.name.NameDto;
 import doyenm.zooshell.common.name.NameValidator;
 import lombok.RequiredArgsConstructor;
 
+import javax.print.StreamPrintService;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 /**
- *
  * @author doyenm
  */
 @RequiredArgsConstructor
 public class KeeperCreationValidator implements Predicate<KeeperCreationContext> {
-    
+
     private final NameValidator nameValidator;
     private final KeepersNumberPredicate keepersNumberPredicate;
-    
+
     @Override
     public boolean test(KeeperCreationContext t) {
-        KeeperCreationContext context = t;
-        
-        return this.keepersNumberPredicate.test(context)
-                && nameValidator.test(NameDto.builder()
-                        .testing(context.getKeeper())
-                        .existingNames(context.getKeepers().keySet())
-                        .build());
+        NameDto dto = NameDto.builder()
+                .testing(t.getKeeper())
+                .existingNames(t.getKeepers().keySet())
+                .build();
+
+        return Stream.of(t)
+                .filter(keepersNumberPredicate::test)
+                .filter(i -> nameValidator.test(dto))
+                .anyMatch(i -> true);
     }
-    
+
 }
