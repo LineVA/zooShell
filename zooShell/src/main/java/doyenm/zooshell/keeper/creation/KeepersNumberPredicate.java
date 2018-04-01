7bbad1b58a75b368edfa7e6a2172aafc76a70f67
Marine <doyenm@carrotCake>
2018-04-01 12:50:00 +0200

    UPDATE Change class to predicate

--- zooShell/src/main/java/doyenm/zooshell/keeper/creation/KeepersNumberPredicate.java
+++ zooShell/src/main/java/doyenm/zooshell/keeper/creation/KeepersNumberPredicate.java
@@ -1,10 +1,12 @@
 package doyenm.zooshell.keeper.creation;
 
+import java.util.function.Predicate;
+
 /**
  *
  * @author doyenm
  */
-public class KeepersNumberPredicate {
+public class KeepersNumberPredicate implements Predicate<KeeperCreationContext>{
 
     public boolean test(KeeperCreationContext context){
         double ratio = context.getPaddocks().size() / 2.0;
