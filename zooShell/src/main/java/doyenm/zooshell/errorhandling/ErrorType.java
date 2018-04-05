91669086b18ecc340f7256aa37ce4136186ab6a5
Marine <doyenm@carrotCake>
2018-04-05 21:01:01 +0200

    ADD Errors on dimensions, speed and horizon

--- zooShell/src/main/java/doyenm/zooshell/errorhandling/ErrorType.java
+++ zooShell/src/main/java/doyenm/zooshell/errorhandling/ErrorType.java
@@ -1,5 +1,7 @@
 package doyenm.zooshell.errorhandling;
 
 public enum ErrorType {
+    INCORRECT_DIMENSIONS,
+    INCORRECT_HORIZON,
     INCORRECT_NAME;
 }
--- zooShell/src/main/java/doyenm/zooshell/zoo/creation/ZooCreationValidator.java
+++ zooShell/src/main/java/doyenm/zooshell/zoo/creation/ZooCreationValidator.java
@@ -30,18 +30,43 @@ public class ZooCreationValidator implements Function<ZooCreationContext, ZooCre
     public ZooCreationContext apply(ZooCreationContext t) {
         ZooCreationContext context = t;
         context.convert();
-        boolean result
-                = this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedWidth(), minWidth);
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHeight(), minHeight);
-        int min = Math.min(context.getConvertedHeight(), context.getConvertedWidth());
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHorizon(), minHorizon);
-        result &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedHorizon(), min);
-        result &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedSpeed(), minSpeed);
-        result &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedSpeed(), maxSpeed);
+        testDimensions(context);
+        testHorizon(context);
+        testSpeed(context);
         testName(context);
         return context;
     }
-    
+
+    private void testDimensions(ZooCreationContext context){
+        boolean isCorrect
+                = this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedWidth(), minWidth);
+        isCorrect &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHeight(), minHeight);
+        if(!isCorrect){
+            context.getErrors().add(new BusinessError(ErrorType.INCORRECT_DIMENSIONS));
+        }
+    }
+
+    private void testHorizon(ZooCreationContext context){
+        boolean isCorrect = true;
+        int max = Math.min(context.getConvertedHeight(), context.getConvertedWidth());
+        isCorrect &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedHorizon(), minHorizon);
+        isCorrect &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedHorizon(), max);
+        if(!isCorrect){
+            context.getErrors().add(new BusinessError(ErrorType.INCORRECT_HORIZON));
+        }
+    }
+
+    private void testSpeed(ZooCreationContext context){
+        boolean isCorrect = true;
+        int max = Math.min(context.getConvertedHeight(), context.getConvertedWidth());
+        isCorrect &= this.integerValuePredicates.mustBeGreaterOrEqualsThan(context.getConvertedSpeed(), minSpeed);
+        isCorrect &= this.integerValuePredicates.mustBeLowerOrEqualsThan(context.getConvertedSpeed(), maxSpeed);
+        if(!isCorrect){
+            context.getErrors().add(new BusinessError(ErrorType.INCORRECT_HORIZON));
+        }
+    }
+
+
     private ZooCreationContext testName(ZooCreationContext context){
         NameDto dto = NameDto.builder()
                 .testing(context.getName())
