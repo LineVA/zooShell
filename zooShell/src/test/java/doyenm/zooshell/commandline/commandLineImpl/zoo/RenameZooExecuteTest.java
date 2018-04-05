759ab6760e6abb0d6bdfb83151808c8edfc680ee
Marine <doyenm@carrotCake>
2018-04-05 22:22:36 +0200

    FIX TEST

--- zooShell/src/test/java/doyenm/zooshell/commandline/commandLineImpl/zoo/RenameZooExecuteTest.java
+++ zooShell/src/test/java/doyenm/zooshell/commandline/commandLineImpl/zoo/RenameZooExecuteTest.java
@@ -1,18 +1,26 @@
 package doyenm.zooshell.commandline.commandLineImpl.zoo;
 
+import doyenm.zooshell.errorhandling.BusinessError;
+import doyenm.zooshell.testUtils.TestUtils;
+import doyenm.zooshell.zoo.creation.ZooCreationContext;
+import doyenm.zooshell.zoo.creation.ZooCreationValidator;
 import doyenm.zooshell.zoo.rename.RenameZoo;
 import doyenm.zooshell.commandline.general.ReturnExec;
 import doyenm.zooshell.commandline.general.TypeReturn;
 import doyenm.zooshell.zoo.ZooContext;
 import doyenm.zooshell.zoo.rename.RenameZooController;
 import doyenm.zooshell.model.Zoo;
+import doyenm.zooshell.zoo.rename.RenameZooValidator;
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
@@ -20,30 +28,67 @@ import static org.mockito.Matchers.any;
  */
 public class RenameZooExecuteTest {
 
-    private RenameZooController givenController() {
-        RenameZooController controller = Mockito.mock(RenameZooController.class);
-        Mockito.when(controller.apply(any(ZooContext.class))).thenAnswer(new Answer<ZooContext>() {
+    private RenameZoo subject;
+    private RenameZooValidator validator;
+    private RenameZooController controller;
+
+    @Before
+    public void setUp(){
+        controller = mock(RenameZooController.class);
+        when(controller.apply(any(ZooContext.class))).thenAnswer(new Answer<ZooContext>() {
             @Override
             public ZooContext answer(InvocationOnMock invocation) throws Throwable {
                 Object[] args = invocation.getArguments();
                 return (ZooContext) args[0];
             }
         });
-        return controller;
+
+        subject = new RenameZoo(validator, controller);
     }
 
     @Test
     public void shouldReturnAReturnExecWithTypeReturnToSuccessWhenTheCommandIsInSuccess() {
         // Given
-        RenameZooController controller = givenController();
-        RenameZoo command = new RenameZoo(null, controller);
+        validator = givenValidator(true);
         String[] cmd = new String[3];
+        subject = new RenameZoo(validator, controller);
         // When
-        ReturnExec result = command.execute(cmd, Mockito.mock(Zoo.class));
+        ReturnExec result = subject.execute(cmd, mock(Zoo.class));
         // Then
         Assertions.assertThat(result).isNotNull();
         Assertions.assertThat(result.getTypeReturn()).isEqualTo(TypeReturn.SUCCESS);
         Assertions.assertThat(result.getMessage()).isNotNull();
         Assertions.assertThat(result.getZoo()).isNotNull();
+    }
+
+    @Test
+    public void shouldReturnAReturnExecWithTypeReturnToFailureWhenTheCommandFailed() {
+        // Given
+        validator = givenValidator(false);
+        String[] cmd = new String[3];
+        subject = new RenameZoo(validator, controller);
+        // When
+        ReturnExec result = subject.execute(cmd, mock(Zoo.class));
+        // Then
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getTypeReturn()).isEqualTo(TypeReturn.ERROR);
+    }
+
+    private RenameZooValidator givenValidator(boolean isOk) {
+        RenameZooValidator validator = mock(RenameZooValidator.class);
+        when(validator.apply(any(ZooContext.class))).thenAnswer(new Answer<ZooContext>() {
+            @Override
+            public ZooContext answer(InvocationOnMock invocation) throws Throwable {
+                Object[] args = invocation.getArguments();
+                if (!isOk) {
+                    ZooContext context = (ZooContext) args[0];
+                    context.getErrors().add(BusinessError.builder().type(TestUtils.getErrorType()).build());
+                    return context;
+                } else {
+                    return (ZooContext) args[0];
+                }
+            }
+        });
+        return validator;
     }
 }
