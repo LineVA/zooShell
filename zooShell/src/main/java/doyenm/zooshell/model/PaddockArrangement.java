99414e01a3a8f47d4d637e94d744125046f148ca
Marine <doyenm@carrotCake>
2018-05-02 21:41:16 +0200

    ADD Add facility "NONE"

--- zooShell/src/main/java/doyenm/zooshell/model/PaddockArrangement.java
+++ zooShell/src/main/java/doyenm/zooshell/model/PaddockArrangement.java
@@ -8,17 +8,18 @@ import lombok.Getter;
  */
 public enum PaddockArrangement {
 
-    BRANCH_CHEW(0),
-    CLIMBING_STRUCTURE(1),
-    DIGGING_ZONE(2),
-    ELEVATED_SLEEPING_ZONE(3),
-    HANGING_ROOF(4),
-    HIBERNATION_ZONE(5),
-    POOL(6),
-    SAND_POOL(7),
-    SCRUB_TREE(8),
-    SETBACK_ZONE(9),
-    SWAMPY_ZONE(10);
+    NONE(0),
+    BRANCH_CHEW(1),
+    CLIMBING_STRUCTURE(2),
+    DIGGING_ZONE(3),
+    ELEVATED_SLEEPING_ZONE(4),
+    HANGING_ROOF(5),
+    HIBERNATION_ZONE(6),
+    POOL(7),
+    SAND_POOL(8),
+    SCRUB_TREE(9),
+    SETBACK_ZONE(10),
+    SWAMPY_ZONE(11);
     
     @Getter
     private int id;
