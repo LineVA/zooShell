b49efa1be32298fce8fa39569d5dc12562ee2ee8
Marine <doyenm@carrotCake>
2018-05-02 22:03:33 +0200

    ADD Command to reset the facilities of a paddock

---
+++ zooShell/src/main/java/doyenm/zooshell/paddock/facilities/ResetPaddockFacilities.java
@@ -0,0 +1,51 @@
+package doyenm.zooshell.paddock.facilities;
+
+import doyenm.zooshell.commandline.general.Command;
+import doyenm.zooshell.commandline.general.ReturnExec;
+import doyenm.zooshell.commandline.general.TypeReturn;
+import doyenm.zooshell.model.Zoo;
+import doyenm.zooshell.paddock.PaddockContext;
+import doyenm.zooshell.paddock.PaddockValidator;
+import doyenm.zooshell.utils.Constants;
+import doyenm.zooshell.utils.DisplayingErrorsList;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * @author doyenm
+ */
+@RequiredArgsConstructor
+public class ResetPaddockFacilities implements Command {
+
+    private final PaddockValidator validator;
+    private final ResetPaddockFacilitiesController controller;
+
+    @Override
+    public ReturnExec execute(String[] cmd, Zoo zoo) {
+        PaddockContext context = new PaddockContext(zoo,
+                cmd[2]);
+        Optional<PaddockContext> optional = Stream.of(context)
+                .filter(validator)
+                .map(controller)
+                .findFirst();
+        if (optional.isPresent()) {
+            return new ReturnExec("RESET_PADDOCK_ARRANGEMENT_SUCCESS", TypeReturn.SUCCESS, context.getZoo());
+        } else {
+            return DisplayingErrorsList.displayErrorList(context.getErrors());
+        }
+    }
+
+    @Override
+    public boolean canExecute(String[] cmd) {
+        return cmd.length == 3
+                && Arrays.asList(Constants.PAD_OR_PADDOCK_ARRANGEMENT)
+                .stream()
+                .anyMatch(cmd[0]::equalsIgnoreCase)
+                && Arrays.asList(Constants.RESET)
+                .stream()
+                .anyMatch(cmd[1]::equalsIgnoreCase);
+    }
+}
---
+++ zooShell/src/test/java/doyenm/zooshell/paddock/facilities/ResetPaddockFacilitiesCanExecuteTest.java
@@ -0,0 +1,90 @@
+package doyenm.zooshell.paddock.facilities;
+
+import org.apache.commons.lang.RandomStringUtils;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+/**
+ * @author doyenm
+ */
+public class ResetPaddockFacilitiesCanExecuteTest {
+
+    private final String PADDOCK = "paddock-arrangement";
+    private final String PAD = "pad-arrangement";
+    private final String RESET = "reset";
+
+    private final ResetPaddockFacilities subject = new ResetPaddockFacilities(null, null);
+
+
+    @Test
+    public void shouldReturnTrueWhenTheCommandIsCorrectAndBeginsWithPaddock() {
+        // Given
+        String[] cmd = {this.PADDOCK, this.RESET,
+                RandomStringUtils.randomAlphabetic(10)};
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenTheCommandIsCorrectAndBeginsWithPad() {
+        // Given
+        String[] cmd = {this.PAD, this.RESET,
+                RandomStringUtils.randomAlphabetic(10)};
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheTheFirstElementIsNotPadOrPaddockArrangement() {
+        // Given
+        String falsePad = RandomStringUtils.randomAlphabetic(10);
+        String[] cmd = {RandomStringUtils.randomAlphabetic(10), this.RESET,
+                RandomStringUtils.randomAlphabetic(10)};
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(falsePad).isNotEqualToIgnoringCase(PADDOCK);
+        Assertions.assertThat(falsePad).isNotEqualToIgnoringCase(PAD);
+        Assertions.assertThat(actualResult).isFalse();
+    }
+
+    @Test
+    public void shouldReturnFalseWhenTheTheSecondElementIsNotRESET() {
+        // Given
+        String falseReset = RandomStringUtils.randomAlphabetic(10);
+        String[] cmd = {this.PADDOCK, falseReset,
+                RandomStringUtils.randomAlphabetic(10), };
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(falseReset).isNotEqualToIgnoringCase(RESET);
+        Assertions.assertThat(actualResult).isFalse();
+    }
+
+    @Test
+    public void shouldReturnFalseWhenThereIsLessThanThreeElements() {
+        // Given
+        String[] cmd = {this.PADDOCK, this.RESET};
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(actualResult).isFalse();
+    }
+
+    @Test
+    public void shouldReturnFalseWhenThereIsMoreThanThreeElements() {
+        // Given
+        String[] cmd = {this.PADDOCK, this.RESET,
+                RandomStringUtils.randomAlphabetic(10),
+                RandomStringUtils.randomAlphabetic(10)};
+        // When
+        boolean actualResult = subject.canExecute(cmd);
+        // Then
+        Assertions.assertThat(actualResult).isFalse();
+    }
+
+}
